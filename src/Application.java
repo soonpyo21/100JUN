d29b92f2fddee8ba5b274cb4b0e4dbed3236d704
soonpyo21 <tnsvy2004@gmail.com>
2022-07-29 00:02:42 +0900

    개

--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,11 @@
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+        // 내가 푼게 아니라 인텔리제이가 풀었다
     }
 }
