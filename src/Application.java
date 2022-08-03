0eab68cc029c40c654785e5ad9998664e8fb1577
soonpyo21 <tnsvy2004@gmail.com>
2022-08-03 23:53:32 +0900

    사칙연산

--- src/Application.java
+++ src/Application.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
 
 // 문제
-// 두 정수 A와 B를 입력받은 다음, A / B를 출력하는 프로그램을 작성하시오.
+// 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
 // 입력
-// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+// 두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
 // 출력
-// 첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10(-9) 이하이면 정답이다.
+// 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
 
 public class Application {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextDouble() / sc.nextDouble());
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
 
     }
 }
