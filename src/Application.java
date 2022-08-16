a7c90385bc156e3de7a5954e1ecbbbf9190684c2
soonpyo21 <tnsvy2004@gmail.com>
2022-08-17 00:18:38 +0900

    나머지

--- src/Application.java
+++ src/Application.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 
 // 문제
-// 동혁이는 오래된 창고를 뒤지다가 낡은 체스판과 피스를 발견했다.
-// 체스판의 먼지를 털어내고 걸레로 닦으니 그럭저럭 쓸만한 체스판이 되었다. 하지만, 검정색 피스는 모두 있었으나, 흰색 피스는 개수가 올바르지 않았다.
-// 체스는 총 16개의 피스를 사용하며, 킹 1개, 퀸 1개, 룩 2개, 비숍 2개, 나이트 2개, 폰 8개로 구성되어 있다.
-// 동혁이가 발견한 흰색 피스의 개수가 주어졌을 때, 몇 개를 더하거나 빼야 올바른 세트가 되는지 구하는 프로그램을 작성하시오.
+// (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+// (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+// 세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄에 동혁이가 찾은 흰색 킹, 퀸, 룩, 비숍, 나이트, 폰의 개수가 주어진다. 이 값은 0보다 크거나 같고 10보다 작거나 같은 정수이다.
+// 첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
 //
 // 출력
-// 첫째 줄에 입력에서 주어진 순서대로 몇 개의 피스를 더하거나 빼야 되는지를 출력한다.
-// 만약 수가 양수라면 동혁이는 그 개수 만큼 피스를 더해야 하는 것이고, 음수라면 제거해야 하는 것이다.
+// 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
 
 public class Application {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println(1 - sc.nextInt());
-        System.out.println(1 - sc.nextInt());
-        System.out.println(2 - sc.nextInt());
-        System.out.println(2 - sc.nextInt());
-        System.out.println(2 - sc.nextInt());
-        System.out.println(8 - sc.nextInt());
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
 
     }
 }
