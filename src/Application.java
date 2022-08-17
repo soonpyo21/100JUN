73df548201e46d41d3f9e5f19be6b377780db827
soonpyo21 <tnsvy2004@gmail.com>
2022-08-18 01:43:02 +0900

    곱셈

--- src/Application.java
+++ src/Application.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 
 // 문제
-// (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
-// (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
-// 세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+// (세 자리 수) × (세 자리 수)는 다음과 같은 과정을 통하여 이루어진다.
+// (1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+// 첫째 줄에 (1)의 위치에 들어갈 세 자리 자연수가, 둘째 줄에 (2)의 위치에 들어갈 세자리 자연수가 주어진다.
 //
 // 출력
-// 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+// 첫째 줄부터 넷째 줄까지 차례대로 (3), (4), (5), (6)에 들어갈 값을 출력한다.
 
 public class Application {
 
@@ -17,13 +16,21 @@ public class Application {
 
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
+        sc.nextLine();
+        String B = sc.nextLine();
+        String[] C =  B.split("");
 
-        System.out.println((A+B)%C);
-        System.out.println(((A%C) + (B%C))%C);
-        System.out.println((A*B)%C);
-        System.out.println(((A%C) * (B%C))%C);
+        System.out.println(A * Integer.parseInt(C[2]));
+        System.out.println(A * Integer.parseInt(C[1]));
+        System.out.println(A * Integer.parseInt(C[0]));
+        System.out.println(A * Integer.parseInt(B));
 
     }
 }
+
+// 모범답안
+//        Scanner s = new Scanner(System.in);
+//        int a,b;
+//        a=s.nextInt();
+//        b=s.nextInt();
+//        System.out.print(a*(b%10)+"\n"+a*((b%100)/10)+"\n"+a*(b/100)+"\n"+a*b);
