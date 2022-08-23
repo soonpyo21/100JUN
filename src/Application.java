ba53a2e64ac30b40f3cef8c89bb0af75ce69a8d3
soonpyo21 <tnsvy2004@gmail.com>
2022-08-24 00:10:56 +0900

    윤년

--- src/Application.java
+++ src/Application.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
 // 문제
-// 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D,
-// 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+// 연도가 주어졌을 때, 윤년이면 1, 아니면 0을 출력하는 프로그램을 작성하시오.
+// 윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
+// 예를 들어, 2012년은 4의 배수이면서 100의 배수가 아니라서 윤년이다.
+// 1900년은 100의 배수이고 400의 배수는 아니기 때문에 윤년이 아니다. 하지만, 2000년은 400의 배수이기 때문에 윤년이다.
 //
 // 입력
-// 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+// 첫째 줄에 연도가 주어진다. 연도는 1보다 크거나 같고, 4000보다 작거나 같은 자연수이다.
 //
 // 출력
-// 시험 성적을 출력한다.
+// 첫째 줄에 윤년이면 1, 아니면 0을 출력한다.
 
 public class Application {
 
@@ -16,24 +18,13 @@ public class Application {
 
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
+        int year = sc.nextInt();
 
-        if(a >= 90) {
-            System.out.println("A");
-        } else if(a < 90 && a >= 80) {
-            System.out.println("B");
-        } else if(a < 80 && a >= 70) {
-            System.out.println("C");
-        } else if(a < 70 && a >= 60) {
-            System.out.println("D");
+        if((year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0)) {
+            System.out.println("1");
         } else {
-            System.out.println("F");
+            System.out.println("0");
         }
 
     }
 }
-
-// 모범 답안
-// interface Main{static void main(String[]z){
-// System.out.print("FFFFFFDCBAA".charAt(new java.util.Scanner(System.in).nextInt()/10));}
-// }
