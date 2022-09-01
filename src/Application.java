ef758f1850ea02f6714e6a4d240afbf482084392
soonpyo21 <tnsvy2004@gmail.com>
2022-09-01 23:58:01 +0900

    A + B - 3

--- src/Application.java
+++ src/Application.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 // 문제
-// N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄에 N이 주어진다. N은 1보다 크거나 같고, 9보다 작거나 같다.
+// 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+// 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
 //
 // 출력
-// 출력형식과 같게 N*1부터 N*9까지 출력한다.
+// 각 테스트 케이스마다 A+B를 출력한다.
 
 public class Application {
 
@@ -15,10 +16,16 @@ public class Application {
 
         Scanner sc = new Scanner(System.in);
 
-        int input = sc.nextInt();
+        int t = sc.nextInt();
+        int[] result = new int[t];
 
-        for(int i = 1; i < 10; i ++) {
-            System.out.println(input + " * " +  i  + " = "  + input * i);
+        for(int i = 0; i < t; i ++) {
+            result[i] = sc.nextInt() + sc.nextInt();
         }
+
+        for(int r : result) {
+            System.out.println(r);
+        }
+
     }
 }
