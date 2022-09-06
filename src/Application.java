05105dbf2e4b9df7554f459b4267aa04758329d4
soonpyo21 <tnsvy2004@gmail.com>
2022-09-07 00:06:48 +0900

    빠른 A+B

--- src/Application.java
+++ src/Application.java
@@ -1,40 +1,39 @@
-import java.util.Scanner;
+import java.io.*;
 
 // 문제
-// 준원이는 저번 주에 살면서 처음으로 코스트코를 가 봤다. 정말 멋졌다. 그런데, 몇 개 담지도 않았는데 수상하게 높은 금액이 나오는 것이다!
-// 준원이는 영수증을 보면서 정확하게 계산된 것이 맞는지 확인해보려 한다.
-// 영수증에 적힌,
-// 1. 구매한 각 물건의 가격과 개수
-// 2. 구매한 물건들의 총 금액
-// 을 보고, 구매한 물건의 가격과 개수로 계산한 총 금액이 영수증에 적힌 총 금액과 일치하는지 검사해보자.
+// 본격적으로 for문 문제를 풀기 전에 주의해야 할 점이 있다. 입출력 방식이 느리면 여러 줄을 입력받거나 출력할 때 시간초과가 날 수 있다는 점이다.
+// C++을 사용하고 있고 cin/cout을 사용하고자 한다면, cin.tie(NULL)과 sync_with_stdio(false)를 둘 다 적용해 주고,
+// endl 대신 개행문자(\n)를 쓰자. 단, 이렇게 하면 더 이상 scanf/printf/puts/getchar/putchar 등 C의 입출력 방식을 사용하면 안 된다.
+// Java를 사용하고 있다면, Scanner와 System.out.println 대신 BufferedReader와 BufferedWriter를 사용할 수 있다.
+// BufferedWriter.flush는 맨 마지막에 한 번만 하면 된다.
+// Python을 사용하고 있다면, input 대신 sys.stdin.readline을 사용할 수 있다.
+// 단, 이때는 맨 끝의 개행문자까지 같이 입력받기 때문에 문자열을 저장하고 싶을 경우 .rstrip()을 추가로 해 주는 것이 좋다.
+// 또한 입력과 출력 스트림은 별개이므로, 테스트케이스를 전부 입력받아서 저장한 뒤 전부 출력할 필요는 없다.
+// 테스트케이스를 하나 받은 뒤 하나 출력해도 된다.
 //
 // 입력
-// 첫째 줄에는 영수증에 적힌 총 금액 X가 주어진다.
-// 둘째 줄에는 영수증에 적힌 구매한 물건의 종류의 수 N이 주어진다.
-// 이후 N개의 줄에는 각 물건의 가격 a와 개수 b가 공백을 사이에 두고 주어진다.
-//
+// 첫 줄에 테스트케이스의 개수 T가 주어진다. T는 최대 1,000,000이다.
+// 다음 T줄에는 각각 두 정수 A와 B가 주어진다. A와 B는 1 이상, 1,000 이하이다.
+
 // 출력
-// 구매한 물건의 가격과 개수로 계산한 총 금액이 영수증에 적힌 총 금액과 일치하면 Yes를 출력한다. 일치하지 않는다면 No를 출력한다.
+// 구각 테스트케이스마다 A+B를 한 줄에 하나씩 순서대로 출력한다.
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int X = sc.nextInt();
-        int N = sc.nextInt();
+        int T = Integer.parseInt(br.readLine());
 
-        int result = 0;
-        for(int i = 0; i < N; i ++) {
-            result += sc.nextInt() * sc.nextInt();
+        for(int i = 0; i < T; i++) {
+            String input = br.readLine();
+            String[] num =  input.split(" ");
+            bw.append(Integer.parseInt(num[0]) + Integer.parseInt(num[1]) + "\n");
         }
-
-        if(X == result) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
-
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }
