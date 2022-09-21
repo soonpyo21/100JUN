1e993ac63898901405cb52c0ad74508dd7898be9
soonpyo21 <tnsvy2004@gmail.com>
2022-09-21 23:12:28 +0900

    별찍기 - 2

--- src/Application.java
+++ src/Application.java
@@ -1,14 +1,14 @@
 import java.io.*;
 
 // 문제
-// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+// 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+// 하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
 //
 // 입력
-// 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
-// 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+// 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
 //
 // 출력
-// 각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+// 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
 
 public class Application {
 
@@ -21,8 +21,12 @@ public class Application {
         String star = "*";
 
         for(int i = 0; i < N; i++) {
+            int place = N - (i + 1);
+            for(int j = 0; j < place; j ++) {
+                star = " " + star;
+            }
             bw.write(star + "\n");
-            star = star + "*";
+            star = star.trim() + "*";
         }
         br.close();
         bw.close();
