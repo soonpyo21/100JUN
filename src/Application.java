db81659157412b4efe35decd5ef099d61f4ff492
soonpyo21 <tnsvy2004@gmail.com>
2022-09-22 23:55:36 +0900

    X보다 작은 수

--- src/Application.java
+++ src/Application.java
@@ -1,14 +1,14 @@
 import java.io.*;
 
 // 문제
-// 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
-// 하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+// 정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+// 첫째 줄에 N과 X가 주어진다. (1 ≤ N, X ≤ 10,000)
+// 둘째 줄에 수열 A를 이루는 정수 N개가 주어진다. 주어지는 정수는 모두 1보다 크거나 같고, 10,000보다 작거나 같은 정수이다.
 //
 // 출력
-// 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+// X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
 
 public class Application {
 
@@ -17,17 +17,18 @@ public class Application {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        String star = "*";
+        String input1 = br.readLine();
+        String[] first = input1.split(" ");
 
-        for(int i = 0; i < N; i++) {
-            int place = N - (i + 1);
-            for(int j = 0; j < place; j ++) {
-                star = " " + star;
+        String input2 = br.readLine();
+        String[] second = input2.split(" ");
+
+        for(int i = 0; i < second.length; i++) {
+            if(Integer.parseInt(second[i]) < Integer.parseInt(first[1])) {
+                bw.write(second[i] + " ");
             }
-            bw.write(star + "\n");
-            star = star.trim() + "*";
         }
+
         br.close();
         bw.close();
     }
