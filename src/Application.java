901dc4fa974172731e571bd1020680109fff49c6
soonpyo21 <tnsvy2004@gmail.com>
2022-09-24 18:28:36 +0900

    A+B - 5

--- src/Application.java
+++ src/Application.java
@@ -1,14 +1,15 @@
 import java.io.*;
 
 // 문제
-// 정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄에 N과 X가 주어진다. (1 ≤ N, X ≤ 10,000)
-// 둘째 줄에 수열 A를 이루는 정수 N개가 주어진다. 주어지는 정수는 모두 1보다 크거나 같고, 10,000보다 작거나 같은 정수이다.
+// 입력은 여러 개의 테스트 케이스로 이루어져 있다.
+// 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+// 입력의 마지막에는 0 두 개가 들어온다.
 //
 // 출력
-// X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
+// 각 테스트 케이스마다 A+B를 출력한다.
 
 public class Application {
 
@@ -17,15 +18,14 @@ public class Application {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String input1 = br.readLine();
-        String[] first = input1.split(" ");
+        while(true) {
+            String num = br.readLine();
+            String[] arr = num.split(" ");
 
-        String input2 = br.readLine();
-        String[] second = input2.split(" ");
-
-        for(int i = 0; i < second.length; i++) {
-            if(Integer.parseInt(second[i]) < Integer.parseInt(first[1])) {
-                bw.write(second[i] + " ");
+            if(arr[0].equals("0") && arr[1].equals("0")) {
+                break;
+            } else {
+                bw.write(Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]) + "\n");
             }
         }
 
