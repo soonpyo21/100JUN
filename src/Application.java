e816a6ce90e1c0592dfb131c08fc333192d025b3
soonpyo21 <tnsvy2004@gmail.com>
2022-09-25 23:56:58 +0900

    A+B - 4

--- src/Application.java
+++ src/Application.java
@@ -6,7 +6,6 @@ import java.io.*;
 // 입력
 // 입력은 여러 개의 테스트 케이스로 이루어져 있다.
 // 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
-// 입력의 마지막에는 0 두 개가 들어온다.
 //
 // 출력
 // 각 테스트 케이스마다 A+B를 출력한다.
@@ -20,15 +19,13 @@ public class Application {
 
         while(true) {
             String num = br.readLine();
-            String[] arr = num.split(" ");
-
-            if(arr[0].equals("0") && arr[1].equals("0")) {
-                break;
-            } else {
+            if(num != null && !num.equals("")) {
+                String[] arr = num.split(" ");
                 bw.write(Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]) + "\n");
+            } else {
+                break;
             }
         }
-
         br.close();
         bw.close();
     }
