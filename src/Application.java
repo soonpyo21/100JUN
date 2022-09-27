9cc0a5f2242a4eaf7b15cfd1b6687d9b08330b79
soonpyo21 <tnsvy2004@gmail.com>
2022-09-27 23:09:34 +0900

    더하기 사이클

--- src/Application.java
+++ src/Application.java
@@ -1,14 +1,20 @@
 import java.io.*;
 
 // 문제
-// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+// 0보다 크거나 같고, 99보다 작거나 같은 정수가 주어질 때 다음과 같은 연산을 할 수 있다.
+// 먼저 주어진 수가 10보다 작다면 앞에 0을 붙여 두 자리 수로 만들고, 각 자리의 숫자를 더한다.
+// 그 다음, 주어진 수의 가장 오른쪽 자리 수와 앞에서 구한 합의 가장 오른쪽 자리 수를 이어 붙이면 새로운 수를 만들 수 있다.
+// 다음 예를 보자.
+// 26부터 시작한다. 2+6 = 8이다. 새로운 수는 68이다. 6+8 = 14이다. 새로운 수는 84이다.
+// 8+4 = 12이다. 새로운 수는 42이다. 4+2 = 6이다. 새로운 수는 26이다.
+// 위의 예는 4번만에 원래 수로 돌아올 수 있다. 따라서 26의 사이클의 길이는 4이다.
+// N이 주어졌을 때, N의 사이클의 길이를 구하는 프로그램을 작성하시오.
 //
 // 입력
-// 입력은 여러 개의 테스트 케이스로 이루어져 있다.
-// 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+// 첫째 줄에 N이 주어진다. N은 0보다 크거나 같고, 99보다 작거나 같은 정수이다.
 //
 // 출력
-// 각 테스트 케이스마다 A+B를 출력한다.
+// 첫째 줄에 N의 사이클 길이를 출력한다.
 
 public class Application {
 
@@ -17,13 +23,27 @@ public class Application {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        String num = br.readLine();
+        int N = 0;
+        int changeN = 0;
+        int cnt = 1;
+
+        if(Integer.parseInt(num) < 10) {
+            N = Integer.parseInt("0" + num);
+        } else {
+            N = Integer.parseInt(num);
+        }
+
+        changeN = N;
         while(true) {
-            String num = br.readLine();
-            if(num != null && !num.equals("")) {
-                String[] arr = num.split(" ");
-                bw.write(Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]) + "\n");
-            } else {
+            int su = ((changeN % 10) * 10) + (((changeN / 10) + (changeN % 10)) % 10);
+
+            if(N == su) {
+                bw.write(cnt + "");
                 break;
+            } else {
+                changeN = su;
+                cnt ++;
             }
         }
         br.close();
