d05c570882f3baec96f85424b4faff6e56e0755a
soonpyo21 <tnsvy2004@gmail.com>
2022-09-29 01:23:40 +0900

    최소, 최대

--- src/Application.java
+++ src/Application.java
@@ -1,20 +1,15 @@
 import java.io.*;
+import java.util.Arrays;
 
 // 문제
-// 0보다 크거나 같고, 99보다 작거나 같은 정수가 주어질 때 다음과 같은 연산을 할 수 있다.
-// 먼저 주어진 수가 10보다 작다면 앞에 0을 붙여 두 자리 수로 만들고, 각 자리의 숫자를 더한다.
-// 그 다음, 주어진 수의 가장 오른쪽 자리 수와 앞에서 구한 합의 가장 오른쪽 자리 수를 이어 붙이면 새로운 수를 만들 수 있다.
-// 다음 예를 보자.
-// 26부터 시작한다. 2+6 = 8이다. 새로운 수는 68이다. 6+8 = 14이다. 새로운 수는 84이다.
-// 8+4 = 12이다. 새로운 수는 42이다. 4+2 = 6이다. 새로운 수는 26이다.
-// 위의 예는 4번만에 원래 수로 돌아올 수 있다. 따라서 26의 사이클의 길이는 4이다.
-// N이 주어졌을 때, N의 사이클의 길이를 구하는 프로그램을 작성하시오.
+// N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄에 N이 주어진다. N은 0보다 크거나 같고, 99보다 작거나 같은 정수이다.
+// 첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다.
+// 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
 //
 // 출력
-// 첫째 줄에 N의 사이클 길이를 출력한다.
+// 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
 
 public class Application {
 
@@ -23,29 +18,19 @@ public class Application {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String num = br.readLine();
-        int N = 0;
-        int changeN = 0;
-        int cnt = 1;
+        String N = br.readLine();
+        int[] arr = new int[Integer.parseInt(N)];
 
-        if(Integer.parseInt(num) < 10) {
-            N = Integer.parseInt("0" + num);
-        } else {
-            N = Integer.parseInt(num);
+        String su = br.readLine();
+        String[] sArr = su.split(" ");
+
+        for(int i = 0; i < arr.length; i ++) {
+            arr[i] = Integer.parseInt(sArr[i]);
         }
 
-        changeN = N;
-        while(true) {
-            int su = ((changeN % 10) * 10) + (((changeN / 10) + (changeN % 10)) % 10);
+        Arrays.sort(arr);
+        bw.write(arr[0] + " " + arr[Integer.parseInt(N) - 1]);
 
-            if(N == su) {
-                bw.write(cnt + "");
-                break;
-            } else {
-                changeN = su;
-                cnt ++;
-            }
-        }
         br.close();
         bw.close();
     }
