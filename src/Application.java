7455002e1093a34bf0f8bebb2dbadc7444e1ff57
soonpyo21 <tnsvy2004@gmail.com>
2022-10-03 00:45:18 +0900

    최댓값

--- src/Application.java
+++ src/Application.java
@@ -1,15 +1,16 @@
 import java.io.*;
-import java.util.Arrays;
 
 // 문제
-// N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+// 9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
+// 예를 들어, 서로 다른 9개의 자연수
+// 3, 29, 38, 12, 57, 74, 40, 85, 61
+// 이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
 //
 // 입력
-// 첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다.
-// 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+// 첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
 //
 // 출력
-// 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+// 첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
 
 public class Application {
 
@@ -18,18 +19,18 @@ public class Application {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String N = br.readLine();
-        int[] arr = new int[Integer.parseInt(N)];
+        int[] num = new int[9];
+        int max = 0;
+        int index = 0;
 
-        String su = br.readLine();
-        String[] sArr = su.split(" ");
-
-        for(int i = 0; i < arr.length; i ++) {
-            arr[i] = Integer.parseInt(sArr[i]);
+        for(int i = 0; i < 9; i ++) {
+            num[i] = Integer.parseInt(br.readLine());
+            if(num[i] > max) {
+                max = num[i];
+                index = i + 1;
+            }
         }
-
-        Arrays.sort(arr);
-        bw.write(arr[0] + " " + arr[Integer.parseInt(N) - 1]);
+        bw.write(max + "\n" + index);
 
         br.close();
         bw.close();
