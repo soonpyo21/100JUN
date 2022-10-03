1d4266c749288917bb269c7193b0340015fa5388
soonpyo21 <tnsvy2004@gmail.com>
2022-10-03 14:14:35 +0900

    나머지

--- src/Application.java
+++ src/Application.java
@@ -1,16 +1,16 @@
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 
 // 문제
-// 9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
-// 예를 들어, 서로 다른 9개의 자연수
-// 3, 29, 38, 12, 57, 74, 40, 85, 61
-// 이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
+// 두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다.
+// 수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
+// 첫째 줄부터 열번째 줄 까지 숫자가 한 줄에 하나씩 주어진다. 이 숫자는 1,000보다 작거나 같고, 음이 아닌 정수이다.
 //
 // 출력
-// 첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
+// 첫째 줄에, 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력한다.
 
 public class Application {
 
@@ -19,20 +19,38 @@ public class Application {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] num = new int[9];
-        int max = 0;
-        int index = 0;
-
-        for(int i = 0; i < 9; i ++) {
-            num[i] = Integer.parseInt(br.readLine());
-            if(num[i] > max) {
-                max = num[i];
-                index = i + 1;
-            }
+        int[] remainder = new int[10];
+        for(int i = 0; i < 10; i ++) {
+            int num = Integer.parseInt(br.readLine());
+            remainder[i] = num % 42;
         }
-        bw.write(max + "\n" + index);
+
+        Set set = new HashSet<>();
+        for(int i = 0; i < remainder.length; i ++) {
+            set.add(remainder[i]);
+        }
+        bw.write(set.size() + "");
 
         br.close();
         bw.close();
     }
 }
+
+// 모범답안
+
+//import java.io.*;
+//import java.util.HashSet;
+//
+//public class Main {
+//    public static void main(String args[]) throws IOException {
+//        InputStream in = System.in;
+//        InputStreamReader reader = new InputStreamReader(in);
+//        BufferedReader br = new BufferedReader(reader);
+//        HashSet<Integer> remainderSet = new HashSet<>();
+//        for(int i=0; i<10; i++){
+//            int input = Integer.valueOf(br.readLine());
+//            remainderSet.add(input%42);
+//        }
+//        System.out.println(remainderSet.size());
+//    }
+//}
