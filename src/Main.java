415ee3c6ae9557b96b043c310f046491c40e4d01
soonpyo21 <tnsvy2004@gmail.com>
2022-11-05 18:45:35 +0900

    과제 안 내신 분..?

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 import java.io.*;
 
 // 문제
-// 총 N개의 정수가 주어졌을 때, 정수 v가 몇 개인지 구하는 프로그램을 작성하시오.
+// X대학 M교수님은 프로그래밍 수업을 맡고 있다. 교실엔 학생이 30명이 있는데, 학생 명부엔 각 학생별로 1번부터 30번까지 출석번호가 붙어 있다.
+// 교수님이 내준 특별과제를 28명이 제출했는데, 그 중에서 제출 안 한 학생 2명의 출석번호를 구하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄에 정수의 개수 N(1 ≤ N ≤ 100)이 주어진다. 둘째 줄에는 정수가 공백으로 구분되어져있다.
-// 셋째 줄에는 찾으려고 하는 정수 v가 주어진다. 입력으로 주어지는 정수와 v는 -100보다 크거나 같으며, 100보다 작거나 같다.
+// 입력은 총 28줄로 각 제출자(학생)의 출석번호 n(1 ≤ n ≤ 30)가 한 줄에 하나씩 주어진다. 출석번호에 중복은 없다.
 //
 // 출력
-// 첫째 줄에 입력으로 주어진 N개의 정수 중에 v가 몇 개인지 출력한다.
+// 출력은 2줄이다. 1번째 줄엔 제출하지 않은 학생의 출석번호 중 가장 작은 것을 출력하고, 2번째 줄에선 그 다음 출석번호를 출력한다.
 
 public class Main {
 
@@ -17,30 +17,53 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] arr = new int[Integer.parseInt(br.readLine())];
-        String[] numbers = br.readLine().split(" ");
-
-        for(int i = 0; i < arr.length; i ++) {
-            arr[i] = Integer.parseInt(numbers[i]);
+        int[] aNumbers = new int[28];
+        for(int i = 0; i < 28; i ++) {
+            aNumbers[i] = Integer.parseInt(br.readLine());
         }
 
-        int v = Integer.parseInt(br.readLine());
-        int cnt = 0;
-
-        for(int num : arr) {
-            if(num == v) {
-                cnt++;
+        int[] notSubNumbers = new int[2];
+        int index = 0;
+        for(int i = 1; i < 31; i ++) {
+            int cnt = 0;
+            for(int j = 0; j < aNumbers.length; j++) {
+                if(i == aNumbers[j]) {
+                    cnt++;
+                }
+            }
+            if(cnt == 0) {
+                notSubNumbers[index] = i;
+                index ++;
             }
         }
 
-        bw.write(cnt + "");
+        if(notSubNumbers[0] > notSubNumbers[1]) {
+            bw.write(notSubNumbers[1] + "\n" + notSubNumbers[0]);
+        } else {
+            bw.write(notSubNumbers[0] + "\n" + notSubNumbers[1]);
+        }
 
         br.close();
         bw.close();
-
-
     }
-
-
 }
 
+// 모범 답안
+//
+//import java.io.*;
+//import java.util.*;
+//
+//public class Main {
+//
+//    public static void main(String[] args) throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int stu[] = new int[31];
+//        for(int i = 0 ; i < 28 ; i++) {
+//            stu[Integer.parseInt(br.readLine())] = 1;
+//        }
+//        for(int i = 1 ; i <= 30 ; i++) {
+//            if(stu[i] == 0) System.out.println(i);
+//        }
+//    }
+//
+//}
