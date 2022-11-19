df881ad1c2e3dd78fcf51b8b608763976610c133
soonpyo21 <tnsvy2004@gmail.com>
2022-11-20 00:04:49 +0900

    단어의 개수(2)

--- src/Main.java
+++ src/Main.java
@@ -19,13 +19,14 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] str =  br.readLine().split(" ");
-
-        if(str[0] != null && !str[0].equals("")) {
-            bw.write(str.length + "");
-        } else {
-            bw.write(str.length-1 + "");
+        int cnt = 0;
+        for(int i = 0; i < str.length; i ++) {
+            if(str[i] != null && !str[i].equals("")) {
+                cnt ++;
+            }
         }
 
+        bw.write(cnt + "");
         br.close();
         bw.close();
     }
