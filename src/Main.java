079bd79dda1722113ccedd0df80c8c26a4f9be87
soonpyo21 <tnsvy2004@gmail.com>
2023-01-03 23:58:42 +0900

    그리디 알고리즘 - 1이 될 떄 까지

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 // 문제
-// 그룹 단어란 단어에 존재하는 모든 문자에 대해서, 각 문자가 연속해서 나타나는 경우만을 말한다.
-// 예를 들면, ccazzzzbb는 c, a, z, b가 모두 연속해서 나타나고, kin도 k, i, n이 연속해서 나타나기 때문에 그룹 단어이지만,
-// aabbbccb는 b가 떨어져서 나타나기 때문에 그룹 단어가 아니다.
-// 단어 N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램을 작성하시오.
+// 어떠한 수 N이 1이 될 때까지 다음의 두 과정 중 하나를 반복적으로 선택하여 수행하려 한다. 단, 두번째 연산은 N이 K로 나누어 떨어질 때만 선택할 수 있다.
+// 1. N에서 1을 뺀다.
+// 2. N을 K로 나눈다.
+// 예를 들어 N이 17, K가 4라고 가정한다. 이떄 1번의 과정을 한번 수행하면 N은 16이 된다. 이후에 2번의 과정을 두 번 수행하면 N은 1이 된다.
+// 결과적으로 이 경우 전체 과정을 실행한 횟수는 3이 된다. 이는 N을 1로 만드는 최소 횟수이다.
+// N과 K가 주어질 때 N이 1이 될 때까지 1번 혹은 2번의 과정을 수행해야 하는 최소 횟수를 구하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄에 단어의 개수 N이 들어온다. N은 100보다 작거나 같은 자연수이다. 둘째 줄부터 N개의 줄에 단어가 들어온다.
-// 단어는 알파벳 소문자로만 되어있고 중복되지 않으며, 길이는 최대 100이다.
+// 첫째 줄에 (N<1<=N<=100,000)과 K(2<=K<=100,000)가 공백을 기준으로 하여 각각 자연수로 주어진다.
 //
 // 출력
-// 첫째 줄에 그룹 단어의 개수를 출력한다.
+// 첫째 줄에 N이 1이 될 때까지 1번 혹은 2번의 과정을 수행해야 하는 횟수의 최솟값을 출력한다.
 
 public class Main {
 
@@ -22,31 +23,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        int groupWordCnt = 0;
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+        int cnt = 0;
 
-        for(int i = 0; i < N; i ++) {
-            char[] word = br.readLine().toCharArray();
-            int uniqueCnt = 1;
-            Set<String> uniqueWordSet = new HashSet<>();
-
-            for(int j = 0; j < word.length; j ++) {
-                if(word.length != 1) {
-                    if(j != word.length-1) {
-                        if(word[j] != word[j+1]) {
-                            uniqueCnt ++;
-                        }
-                    }
-                }
-                uniqueWordSet.add(String.valueOf(word[j]));
-            }
-
-            if(uniqueCnt == uniqueWordSet.size()) {
-                groupWordCnt ++;
+        while(N != 1) {
+            if(N % K != 0) {
+                N = N - 1;
+                cnt ++;
+            } else {
+                N = N / K;
+                cnt ++;
             }
         }
 
-        bw.write(groupWordCnt + "");
+        bw.write(cnt + " ");
         br.close();
         bw.close();
     }
