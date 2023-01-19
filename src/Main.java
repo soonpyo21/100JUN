6daa426b5166ba7ad15c0a56ff175b0ebc5cd715
soonpyo21 <tnsvy2004@gmail.com>
2023-01-19 09:49:04 +0900

    손익분기점(1) - 시간 초과

--- src/Main.java
+++ src/Main.java
@@ -2,44 +2,45 @@ import java.io.*;
 import java.util.Scanner;
 
 // 문제
-// 어떠한 수 N이 1이 될 때까지 다음의 두 과정 중 하나를 반복적으로 선택하여 수행하려 한다. 단, 두번째 연산은 N이 K로 나누어 떨어질 때만 선택할 수 있다.
-// 1. N에서 1을 뺀다.
-// 2. N을 K로 나눈다.
-// 예를 들어 N이 17, K가 4라고 가정한다. 이떄 1번의 과정을 한번 수행하면 N은 16이 된다. 이후에 2번의 과정을 두 번 수행하면 N은 1이 된다.
-// 결과적으로 이 경우 전체 과정을 실행한 횟수는 3이 된다. 이는 N을 1로 만드는 최소 횟수이다.
-// N과 K가 주어질 때 N이 1이 될 때까지 1번 혹은 2번의 과정을 수행해야 하는 최소 횟수를 구하는 프로그램을 작성하시오.
+// 월드전자는 노트북을 제조하고 판매하는 회사이다. 노트북 판매 대수에 상관없이 매년 임대료, 재산세, 보험료, 급여 등 A만원의 고정 비용이 들며, 
+// 한 대의 노트북을 생산하는 데에는 재료비와 인건비 등 총 B만원의 가변 비용이 든다고 한다.
+// 예를 들어 A=1,000, B=70이라고 하자. 이 경우 노트북을 한 대 생산하는 데는 총 1,070만원이 들며, 열 대 생산하는 데는 총 1,700만원이 든다.
+// 노트북 가격이 C만원으로 책정되었다고 한다. 일반적으로 생산 대수를 늘려 가다 보면 어느 순간 총 수입(판매비용)이 총 비용(=고정비용+가변비용)보다 많아지게 된다. 
+// 최초로 총 수입이 총 비용보다 많아져 이익이 발생하는 지점을 손익분기점(BREAK-EVEN POINT)이라고 한다.
+// A, B, C가 주어졌을 때, 손익분기점을 구하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄에 (N<1<=N<=100,000)과 K(2<=K<=100,000)가 공백을 기준으로 하여 각각 자연수로 주어진다.
+// 첫째 줄에 A, B, C가 빈 칸을 사이에 두고 순서대로 주어진다. A, B, C는 21억 이하의 자연수이다.
 //
 // 출력
-// 첫째 줄에 N이 1이 될 때까지 1번 혹은 2번의 과정을 수행해야 하는 횟수의 최솟값을 출력한다.
+// 첫 번째 줄에 손익분기점 즉 최초로 이익이 발생하는 판매량을 출력한다. 손익분기점이 존재하지 않으면 -1을 출력한다.
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-
-        // N, K를 공백을 기준으로 구분하여 입력 받기
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-        int result = 0;
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] input = br.readLine().split(" ");
+        int fixedCost = Integer.parseInt(input[0]);
+        int variableCost = Integer.parseInt(input[1]);
+        int price = Integer.parseInt(input[2]);
+        long cnt = 0;
+        
         while(true) {
-            // N이 K로 나누어 떨어지는 수가 될 때까지 빼기
-            int target = (N/K) * K;
-            result += (N - target);
-            N = target;
-            // N이 K보다 작을 때, (더 이상 나눌 수 없을 때) 반복문 탈출
-            if(N < K)  break;
-            // K로 나누기
-            result += 1;
-            N /= K;
+            cnt ++;
+            if(fixedCost + (variableCost * cnt) < price * cnt) {
+                break;
+            }
+            if(cnt > 2100000000) {
+                cnt = -1;
+                break;
+            }
         }
-        // 마지막으로 남은 수에 대하여 1씩 빼기
-        result += (N-1);
-        System.out.println(result);
-
+        
+        bw.write(cnt + "");
+        br.close();
+        bw.close();
     }
 }
