d3641bb54341914d666ade3392a0d3829b6df0f7
soonpyo21 <tnsvy2004@gmail.com>
2023-01-20 09:34:37 +0900

    벌집

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,15 @@ import java.io.*;
 import java.util.Scanner;
 
 // 문제
-// 월드전자는 노트북을 제조하고 판매하는 회사이다. 노트북 판매 대수에 상관없이 매년 임대료, 재산세, 보험료, 급여 등 A만원의 고정 비용이 들며, 
-// 한 대의 노트북을 생산하는 데에는 재료비와 인건비 등 총 B만원의 가변 비용이 든다고 한다.
-// 예를 들어 A=1,000, B=70이라고 하자. 이 경우 노트북을 한 대 생산하는 데는 총 1,070만원이 들며, 열 대 생산하는 데는 총 1,700만원이 든다.
-// 노트북 가격이 C만원으로 책정되었다고 한다. 일반적으로 생산 대수를 늘려 가다 보면 어느 순간 총 수입(판매비용)이 총 비용(=고정비용+가변비용)보다 많아지게 된다. 
-// 최초로 총 수입이 총 비용보다 많아져 이익이 발생하는 지점을 손익분기점(BREAK-EVEN POINT)이라고 한다.
-// A, B, C가 주어졌을 때, 손익분기점을 구하는 프로그램을 작성하시오.
+// 위의 그림과 같이 육각형으로 이루어진 벌집이 있다. 그림에서 보는 바와 같이 중앙의 방 1부터 시작해서 이웃하는 방에 돌아가면서 1씩 증가하는 번호를 주소로 매길 수 있다. 
+// 숫자 N이 주어졌을 때, 벌집의 중앙 1에서 N번 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나가는지(시작과 끝을 포함하여)를 계산하는 프로그램을 작성하시오. 
+// 예를 들면, 13까지는 3개, 58까지는 5개를 지난다.
 //
 // 입력
-// 첫째 줄에 A, B, C가 빈 칸을 사이에 두고 순서대로 주어진다. A, B, C는 21억 이하의 자연수이다.
+// 첫째 줄에 N(1 ≤ N ≤ 1,000,000,000)이 주어진다.
 //
 // 출력
-// 첫 번째 줄에 손익분기점 즉 최초로 이익이 발생하는 판매량을 출력한다. 손익분기점이 존재하지 않으면 -1을 출력한다.
+// 입력으로 주어진 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나는지 출력한다.
 
 public class Main {
 
@@ -21,20 +18,20 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int input = Integer.parseInt(br.readLine());
+        int range = 1;
+        int cnt = 1;
         
-        String[] input = br.readLine().split(" ");
-        double fixedCost = Integer.parseInt(input[0]);
-        double variableCost = Integer.parseInt(input[1]);
-        double price = Integer.parseInt(input[2]);
-        double cnt = 0;
-        
-        if(variableCost < price) {
-            cnt = fixedCost / (price - variableCost) + 1;
-        } else {
-            cnt = -1;
+        while(true) {
+            if(input <= range) {
+                break;
+            }
+            range += 6 * cnt;
+            cnt ++;
         }
         
-        bw.write((long) cnt + "");
+        bw.write(cnt + "");
         br.close();
         bw.close();
     }
