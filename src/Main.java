4563fc9e26a232bf76664c635845be4f3d2e82d3
soonpyo21 <tnsvy2004@gmail.com>
2023-01-21 17:44:57 +0900

    분수찾기 - 클론코딩

--- src/Main.java
+++ src/Main.java
@@ -2,44 +2,62 @@ import java.io.*;
 import java.util.Scanner;
 
 // 문제
-// 어떠한 수 N이 1이 될 때까지 다음의 두 과정 중 하나를 반복적으로 선택하여 수행하려 한다. 단, 두번째 연산은 N이 K로 나누어 떨어질 때만 선택할 수 있다.
-// 1. N에서 1을 뺀다.
-// 2. N을 K로 나눈다.
-// 예를 들어 N이 17, K가 4라고 가정한다. 이떄 1번의 과정을 한번 수행하면 N은 16이 된다. 이후에 2번의 과정을 두 번 수행하면 N은 1이 된다.
-// 결과적으로 이 경우 전체 과정을 실행한 횟수는 3이 된다. 이는 N을 1로 만드는 최소 횟수이다.
-// N과 K가 주어질 때 N이 1이 될 때까지 1번 혹은 2번의 과정을 수행해야 하는 최소 횟수를 구하는 프로그램을 작성하시오.
+// 무한히 큰 배열에 다음과 같이 분수들이 적혀있다.
+//
+//  1/1	1/2	1/3	1/4	1/5	…
+//  2/1	2/2	2/3	2/4	…	…
+//  3/1	3/2	3/3	…	…	…
+//  4/1	4/2	…	…	…	…
+//  5/1	…	…	…	…	…
+//  …	…	…	…	…	…
+//
+// 이와 같이 나열된 분수들을 1/1 → 1/2 → 2/1 → 3/1 → 2/2 → … 과 같은 지그재그 순서로 차례대로 1번, 2번, 3번, 4번, 5번, … 분수라고 하자.
+// X가 주어졌을 때, X번째 분수를 구하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄에 (N<1<=N<=100,000)과 K(2<=K<=100,000)가 공백을 기준으로 하여 각각 자연수로 주어진다.
+// 첫째 줄에 X(1 ≤ X ≤ 10,000,000)가 주어진다.
 //
 // 출력
-// 첫째 줄에 N이 1이 될 때까지 1번 혹은 2번의 과정을 수행해야 하는 횟수의 최솟값을 출력한다.
+// 첫째 줄에 분수를 출력한다.
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // N, K를 공백을 기준으로 구분하여 입력 받기
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-        int result = 0;
+        // 1,1 1,2 2,1 3,1 2,2 1,3 1,4 2,3 3,2
+
+        int input = Integer.parseInt(br.readLine());
+        int crossCnt = 1;       // 해당 범위의 대각선 칸 개수
+        int prev_cnt_sum = 0;   // 해당 대각선 직전 대각선까지의 칸의 누적 합
 
         while(true) {
-            // N이 K로 나누어 떨어지는 수가 될 때까지 빼기
-            int target = (N/K) * K;
-            result += (N - target);
-            N = target;
-            // N이 K보다 작을 때, (더 이상 나눌 수 없을 때) 반복문 탈출
-            if(N < K)  break;
-            // K로 나누기
-            result += 1;
-            N /= K;
+
+            // 직전 대각선 누적합 + 해당 대각선 개수 이용한 범위 판별
+            if(input <= crossCnt + prev_cnt_sum) {
+
+                if(crossCnt % 2 == 1) { // 대각선의 개수가 홀수라면
+                    // 분자가 큰 수부터 시작
+                    // 분자는 대각선상 내의 블럭 개수 - (X번째 - 직전 대각선까지의 블럭 개수 -1)
+                    // 분모는 X번째 - 직전 대각선까지의 블럭 개수
+                    bw.write((crossCnt - (input - prev_cnt_sum - 1)) + "/" + (input - prev_cnt_sum));
+                    break;
+                } else { // 대각선상의 블럭의 개수가 짝수라면
+                    // 홀수일 때의 출력을 반대로
+                    bw.write((input - prev_cnt_sum) + "/" + (crossCnt - (input - prev_cnt_sum - 1)));
+                    break;
+                }
+            } else {
+                prev_cnt_sum += crossCnt;
+                crossCnt ++;
+            }
         }
-        // 마지막으로 남은 수에 대하여 1씩 빼기
-        result += (N-1);
-        System.out.println(result);
+
+        br.close();
+        bw.close();
 
     }
 }
+
