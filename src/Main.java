8fe301cc1baa2684a35f952dc52f6853f41c1459
soonpyo21 <tnsvy2004@gmail.com>
2023-01-25 10:31:16 +0900

    달팽이는 올라가고 싶다 (1) - 시간초과

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,15 @@ import java.io.*;
 import java.util.Scanner;
 
 // 문제
-// 무한히 큰 배열에 다음과 같이 분수들이 적혀있다.
-//
-//  1/1	1/2	1/3	1/4	1/5	…
-//  2/1	2/2	2/3	2/4	…	…
-//  3/1	3/2	3/3	…	…	…
-//  4/1	4/2	…	…	…	…
-//  5/1	…	…	…	…	…
-//  …	…	…	…	…	…
-//
-// 이와 같이 나열된 분수들을 1/1 → 1/2 → 2/1 → 3/1 → 2/2 → … 과 같은 지그재그 순서로 차례대로 1번, 2번, 3번, 4번, 5번, … 분수라고 하자.
-// X가 주어졌을 때, X번째 분수를 구하는 프로그램을 작성하시오.
+// 땅 위에 달팽이가 있다. 이 달팽이는 높이가 V미터인 나무 막대를 올라갈 것이다.
+// 달팽이는 낮에 A미터 올라갈 수 있다. 하지만, 밤에 잠을 자는 동안 B미터 미끄러진다. 또, 정상에 올라간 후에는 미끄러지지 않는다.
+// 달팽이가 나무 막대를 모두 올라가려면, 며칠이 걸리는지 구하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄에 X(1 ≤ X ≤ 10,000,000)가 주어진다.
+// 첫째 줄에 세 정수 A, B, V가 공백으로 구분되어서 주어진다. (1 ≤ B < A ≤ V ≤ 1,000,000,000)
 //
 // 출력
-// 첫째 줄에 분수를 출력한다.
+// 첫째 줄에 달팽이가 나무 막대를 모두 올라가는데 며칠이 걸리는지 출력한다.
 
 public class Main {
 
@@ -27,34 +19,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 1,1 1,2 2,1 3,1 2,2 1,3 1,4 2,3 3,2
-
-        int input = Integer.parseInt(br.readLine());
-        int crossCnt = 1;       // 해당 범위의 대각선 칸 개수
-        int prev_cnt_sum = 0;   // 해당 대각선 직전 대각선까지의 칸의 누적 합
-
+        String[] arr = br.readLine().split(" ");
+        long A = Integer.parseInt(arr[0]);
+        long B = Integer.parseInt(arr[1]);
+        long V = Integer.parseInt(arr[2]);
+        
+        long meter = 0;
+        long day = 0;
+        
         while(true) {
-
-            // 직전 대각선 누적합 + 해당 대각선 개수 이용한 범위 판별
-            if(input <= crossCnt + prev_cnt_sum) {
-
-                if(crossCnt % 2 == 1) { // 대각선의 개수가 홀수라면
-                    // 분자가 큰 수부터 시작
-                    // 분자는 대각선상 내의 블럭 개수 - (X번째 - 직전 대각선까지의 블럭 개수 -1)
-                    // 분모는 X번째 - 직전 대각선까지의 블럭 개수
-                    bw.write((crossCnt - (input - prev_cnt_sum - 1)) + "/" + (input - prev_cnt_sum));
-                    break;
-                } else { // 대각선상의 블럭의 개수가 짝수라면
-                    // 홀수일 때의 출력을 반대로
-                    bw.write((input - prev_cnt_sum) + "/" + (crossCnt - (input - prev_cnt_sum - 1)));
-                    break;
-                }
-            } else {
-                prev_cnt_sum += crossCnt;
-                crossCnt ++;
+            
+            meter += A;
+            day ++;
+            
+            if(meter >= V) {
+                break;
             }
+            
+            meter -= B;
         }
 
+        bw.write(day + "");
         br.close();
         bw.close();
 
