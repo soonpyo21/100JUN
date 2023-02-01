88db4f9232f4b916de6691c430e6b25c146a7e47
soonpyo21 <tnsvy2004@gmail.com>
2023-02-01 14:35:35 +0900

    소수 찾기

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import java.io.*;
 import java.util.Scanner;
 
 // 문제
-// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+// 주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄에 A와 B가 주어진다. (0 < A,B < 1010000)
+// 첫 줄에 수의 개수 N이 주어진다. N은 100이하이다. 다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다.
 //
 // 출력
-// 첫째 줄에 A+B를 출력한다.
+// 주어진 수들 중 소수의 개수를 출력한다.
 
 public class Main {
 
@@ -17,69 +17,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int N = Integer.parseInt(br.readLine());
         String[] input = br.readLine().split(" ");
-        String[] Aarr = input[0].split("");
-        String[] Barr = input[1].split("");
+        int cnt = 0;
         
-        int gap = 0;
-        int plus = 0;
-        String first = "";
-        
-        if(Aarr.length == Barr.length) {
-            gap = 0;
-        } else {
-            gap = Math.abs(Aarr.length - Barr.length);
-        }
-        
-        String[] newAarr, newBarr, result;
-        
-        if(Aarr.length > Barr.length) {
-            newAarr = Aarr;
-            newBarr = new String[Aarr.length];
+        for(int i = 0; i < N; i ++) {
+            int num = Integer.parseInt(input[i]);
+            boolean isSosu = true;
             
-            for(int i = 0; i < gap; i ++) {
-                newBarr[i] = "0";
-            }
-            for(int i = 0; i < Barr.length; i ++) {
-                newBarr[gap + i] = Barr[i];
-            }
-        } else if (Barr.length > Aarr.length) {
-            newAarr = new String[Barr.length];
-            newBarr = Barr;
-
-            for(int i = 0; i < gap; i ++) {
-                newAarr[i] = "0";
-            }
-            for(int i = 0; i < Aarr.length; i ++) {
-                newAarr[gap + i] = Aarr[i];
-            }
-        } else {
-            newAarr = Aarr;
-            newBarr = Barr;
-        }
-        
-        result = new String[newAarr.length];
-        
-        for(int i = newAarr.length - 1; i >= 0; i --) {
-            int num = Integer.parseInt(newAarr[i]) + Integer.parseInt(newBarr[i]) + plus;
-            
-            if(num >= 10) {
-                if(i == 0) {
-                    first = "1";
-                }
-                result[i] = String.valueOf(String.valueOf(num).charAt(1));
-                plus = 1;
+            if(num == 2 || num == 3 || num == 5 || num == 7) {
+                cnt ++;
             } else {
-                result[i] = String.valueOf(num);
-                plus = 0;
+                for(int j = num - 1; j > 1; j --) {
+                    if(num % j == 0) {
+                        isSosu = false;
+                    }
+                }
+                
+                if(isSosu && num != 1) cnt ++;
             }
-        }    
-        
-        bw.write(first);
-        for(int i = 0; i < result.length; i ++) {
-            bw.write(result[i] + "");
         }
-        
+
+        bw.write(cnt + "");
         br.close();
         bw.close();
     }
