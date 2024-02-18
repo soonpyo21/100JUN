c203771eb9ef5d6cab081d9a52ade69a99201c72
soonpyo21 <tnsvy2004@gmail.com>
2024-02-19 02:40:28 +0900

    프로그래머스 - 가장 많이 받은 선물

--- src/Main.java
+++ src/Main.java
@@ -3,54 +3,164 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 // 문제
-// 인하은행에는 ATM이 1대밖에 없다. 지금 이 ATM앞에 N명의 사람들이 줄을 서있다.
-// 사람은 1번부터 N번까지 번호가 매겨져 있으며, i번 사람이 돈을 인출하는데 걸리는 시간은 Pi분이다.
+// 선물을 직접 전하기 힘들 때 카카오톡 선물하기 기능을 이용해 축하 선물을 보낼 수 있습니다.
+// 당신의 친구들이 이번 달까지 선물을 주고받은 기록을 바탕으로 다음 달에 누가 선물을 많이 받을지 예측하려고 합니다.
 //
-// 사람들이 줄을 서는 순서에 따라서, 돈을 인출하는데 필요한 시간의 합이 달라지게 된다.
-// 예를 들어, 총 5명이 있고, P1 = 3, P2 = 1, P3 = 4, P4 = 3, P5 = 2 인 경우를 생각해보자.
-// [1, 2, 3, 4, 5] 순서로 줄을 선다면, 1번 사람은 3분만에 돈을 뽑을 수 있다.
-// 2번 사람은 1번 사람이 돈을 뽑을 때 까지 기다려야 하기 때문에, 3+1 = 4분이 걸리게 된다.
-// 3번 사람은 1번, 2번 사람이 돈을 뽑을 때까지 기다려야 하기 때문에, 총 3+1+4 = 8분이 필요하게 된다.
-// 4번 사람은 3+1+4+3 = 11분, 5번 사람은 3+1+4+3+2 = 13분이 걸리게 된다.
-// 이 경우에 각 사람이 돈을 인출하는데 필요한 시간의 합은 3+4+8+11+13 = 39분이 된다.
+// 두 사람이 선물을 주고받은 기록이 있다면, 이번 달까지 두 사람 사이에 더 많은 선물을 준 사람이 다음 달에 선물을 하나 받습니다.
+// 예를 들어 A가 B에게 선물을 5번 줬고, B가 A에게 선물을 3번 줬다면 다음 달엔 A가 B에게 선물을 하나 받습니다.
 //
-// 줄을 [2, 5, 1, 4, 3] 순서로 줄을 서면, 2번 사람은 1분만에, 5번 사람은 1+2 = 3분, 1번 사람은 1+2+3 = 6분,
-// 4번 사람은 1+2+3+3 = 9분, 3번 사람은 1+2+3+3+4 = 13분이 걸리게 된다. 각 사람이 돈을 인출하는데 필요한 시간의 합은 1+3+6+9+13 = 32분이다.
-// 이 방법보다 더 필요한 시간의 합을 최소로 만들 수는 없다.
+// 두 사람이 선물을 주고받은 기록이 하나도 없거나 주고받은 수가 같다면, 선물 지수가 더 큰 사람이 선물 지수가 더 작은 사람에게 선물을 하나 받습니다.
+// 선물 지수는 이번 달까지 자신이 친구들에게 준 선물의 수에서 받은 선물의 수를 뺀 값입니다.
+// 예를 들어 A가 친구들에게 준 선물이 3개고 받은 선물이 10개라면 A의 선물 지수는 -7입니다.
+// B가 친구들에게 준 선물이 3개고 받은 선물이 2개라면 B의 선물 지수는 1입니다.
+// 만약 A와 B가 선물을 주고받은 적이 없거나 정확히 같은 수로 선물을 주고받았다면, 다음 달엔 B가 A에게 선물을 하나 받습니다.
+// 만약 두 사람의 선물 지수도 같다면 다음 달에 선물을 주고받지 않습니다.
 //
-// 줄을 서 있는 사람의 수 N과 각 사람이 돈을 인출하는데 걸리는 시간 Pi가 주어졌을 때,
-// 각 사람이 돈을 인출하는데 필요한 시간의 합의 최솟값을 구하는 프로그램을 작성하시오.
+// 위에서 설명한 규칙대로 다음 달에 선물을 주고받을 때, 당신은 선물을 가장 많이 받을 친구가 받을 선물의 수를 알고 싶습니다.
+//
+// 친구들의 이름을 담은 1차원 문자열 배열 friends 이번 달까지 친구들이 주고받은 선물 기록을 담은 1차원 문자열 배열 gifts가 매개변수로 주어집니다.
+// 이때, 다음달에 가장 많은 선물을 받는 친구가 받을 선물의 수를 return 하도록 solution 함수를 완성해 주세요.
+//
+// 제한사항
+// 2 ≤ friends의 길이 = 친구들의 수 ≤ 50
+// friends의 원소는 친구의 이름을 의미하는 알파벳 소문자로 이루어진 길이가 10 이하인 문자열입니다.
+// 이름이 같은 친구는 없습니다.
+//
+// 1 ≤ gifts의 길이 ≤ 10,000
+// gifts의 원소는 "A B"형태의 문자열입니다. A는 선물을 준 친구의 이름을 B는 선물을 받은 친구의 이름을 의미하며 공백 하나로 구분됩니다.
+// A와 B는 friends의 원소이며 A와 B가 같은 이름인 경우는 존재하지 않습니다.
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] friends = {"muzi", "ryan", "frodo", "neo"};
+        String[] gifts = {"muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"};
 
-        int N = Integer.parseInt(br.readLine());
-        String[] input = br.readLine().split(" ");
-        int[] times = new int[N];
-        int minTime = 0;
+        solution(friends, gifts);
 
-        for(int i = 0; i < N; i ++) {
-            times[i] = Integer.parseInt(input[i]);
+    }
+
+    public static int solution(String[] friends, String[] gifts) {
+
+        String[] from = new String[gifts.length];
+        String[] to = new String[gifts.length];
+
+        for(int i = 0; i < gifts.length; i ++) {
+            String[] arr = gifts[i].split(" ");
+            from[i] = arr[0];
+            to[i] = arr[1];
         }
 
-        Arrays.sort(times);
+        int[][] arr = new int[friends.length][friends.length];
 
-        for(int i = 0; i < N; i ++) {
-            minTime += times[i];
-            int num = i;
-            if(num > 0){
-                for(int j = num; j > 0; j --) {
-                    minTime += times[j-1];
+        for(int i = 0; i < friends.length; i ++) {
+
+            for(int j = 0; j < friends.length; j ++) {
+                int cnt = 0;
+                for(int k = 0; k < gifts.length; k ++) {
+                    if(friends[i].equals(from[k]) && friends[j].equals(to[k])) {
+                        cnt ++;
+                    }
                 }
+                arr[i][j] = cnt;
+            }
+
+        }
+
+        int[][] sumArr = new int[friends.length][3];
+
+        for(int i = 0; i < friends.length; i ++) {
+            int sendSum = 0;
+            int reciveSum = 0;
+
+            for(int j = 0; j < friends.length; j ++) {
+                sendSum += arr[i][j];
+                reciveSum += arr[j][i];
+            }
+            sumArr[i][0] = sendSum;
+            sumArr[i][1] = reciveSum;
+            sumArr[i][2] = sendSum - reciveSum;
+        }
+
+        int[] resultArr = new int[friends.length];
+
+        for(int i = 0; i < friends.length; i ++) {
+
+            int giftCnt = 0;
+
+            for(int j = 0; j < friends.length; j ++) {
+
+                if(i != j && arr[i][j] != 0) {
+                    int send = arr[i][j];
+                    int recive = arr[j][i];
+
+                    if(send > recive) {
+                        System.out.println(friends[i] + "가 " + friends[j] + "에게서 선물을 하나 받습니다.");
+                        giftCnt ++;
+                    } else if (send == recive) {
+                        int senderJisoo = sumArr[i][2];
+                        int reciverJisoo = sumArr[j][2];
+
+                        if(senderJisoo > reciverJisoo) {
+                            System.out.println(friends[i] + "가 " + friends[j] + "보다 선물지수가 커 선물을 하나 받습니다.");
+                            giftCnt ++;
+                        }
+                    }
+                } else {
+                    if(i != j && arr[i][j] == 0 && arr[j][i] == 0) {
+                        int senderJisoo = sumArr[i][2];
+                        int reciverJisoo = sumArr[j][2];
+
+                        if(senderJisoo > reciverJisoo) {
+                            System.out.println(friends[i] + "가 " + friends[j] + "보다 선물지수가 커 선물을 하나 받습니다.");
+                            giftCnt ++;
+                        }
+                    }
+                }
+            }
+
+            resultArr[i] = giftCnt;
+        }
+
+        int max = 0;
+        for(int i = 0; i < resultArr.length; i ++) {
+            if(resultArr[i] > max) {
+                max = resultArr[i];
             }
         }
 
-        bw.write(minTime + "");
-        br.close();
-        bw.close();
+        System.out.println();
+
+        for(int i = 0; i < friends.length; i ++) {
+            for(int j = 0; j < friends.length; j ++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < friends.length; i ++) {
+            for(int j = 0; j < 3; j ++) {
+                System.out.print(sumArr[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < friends.length; i ++) {
+            System.out.print(resultArr[i]);
+        }
+
+        System.out.println();
+
+        System.out.println(max);
+
+        int answer = max;
+        return answer;
     }
+
+
 }
