658a5547ca085e2bbf2d8f9e8413c296f42c78a4
soonpyo21 <tnsvy2004@gmail.com>
2024-02-21 23:41:56 +0900

    프로그래머스 - 추억 점수

--- src/Main.java
+++ src/Main.java
@@ -3,62 +3,67 @@ import java.util.HashMap;
 import java.util.Map;
 
 // 문제
-// 얀에서는 매년 달리기 경주가 열립니다.
-// 해설진들은 선수들이 자기 바로 앞의 선수를 추월할 때 추월한 선수의 이름을 부릅니다.
-// 예를 들어 1등부터 3등까지 "mumu", "soe", "poe" 선수들이 순서대로 달리고 있을 때,
-// 해설진이 "soe"선수를 불렀다면 2등인 "soe" 선수가 1등인 "mumu" 선수를 추월했다는 것입니다.
-// 즉 "soe" 선수가 1등, "mumu" 선수가 2등으로 바뀝니다.
+// 사진들을 보며 추억에 젖어 있던 루는 사진별로 추억 점수를 매길려고 합니다.
+// 사진 속에 나오는 인물의 그리움 점수를 모두 합산한 값이 해당 사진의 추억 점수가 됩니다.
+// 예를 들어 사진 속 인물의 이름이 ["may", "kein", "kain"]이고
+// 각 인물의 그리움 점수가 [5점, 10점, 1점]일 때 해당 사진의 추억 점수는 16(5 + 10 + 1)점이 됩니다.
+// 다른 사진 속 인물의 이름이 ["kali", "mari", "don", "tony"]이고
+// ["kali", "mari", "don"]의 그리움 점수가 각각 [11점, 1점, 55점]]이고,
+// "tony"는 그리움 점수가 없을 때, 이 사진의 추억 점수는 3명의 그리움 점수를 합한 67(11 + 1 + 55)점입니다.
 //
-// 선수들의 이름이 1등부터 현재 등수 순서대로 담긴 문자열 배열 players와 해설진이 부른 이름을 담은 문자열 배열 callings가 매개변수로 주어질 때,
-// 경주가 끝났을 때 선수들의 이름을 1등부터 등수 순서대로 배열에 담아 return 하는 solution 함수를 완성해주세요.
+// 그리워하는 사람의 이름을 담은 문자열 배열 name, 각 사람별 그리움 점수를 담은 정수 배열 yearning,
+// 각 사진에 찍힌 인물의 이름을 담은 이차원 문자열 배열 photo가 매개변수로 주어질 때,
+// 사진들의 추억 점수를 photo에 주어진 순서대로 배열에 담아 return하는 solution 함수를 완성해주세요.
 //
 // 제한사항
-// 5 ≤ players의 길이 ≤ 50,000
-// players[i]는 i번째 선수의 이름을 의미합니다.
-// players의 원소들은 알파벳 소문자로만 이루어져 있습니다.
-// players에는 중복된 값이 들어가 있지 않습니다.
-// 3 ≤ players[i]의 길이 ≤ 10
+// 3 ≤ name의 길이 = yearning의 길이≤ 100
+// 3 ≤ name의 원소의 길이 ≤ 7
+// name의 원소들은 알파벳 소문자로만 이루어져 있습니다.
+// name에는 중복된 값이 들어가지 않습니다.
+// 1 ≤ yearning[i] ≤ 100
+// yearning[i]는 i번째 사람의 그리움 점수입니다.
 //
-// 2 ≤ callings의 길이 ≤ 1,000,000
-// callings는 players의 원소들로만 이루어져 있습니다.
-// 경주 진행중 1등인 선수의 이름은 불리지 않습니다.
+// 3 ≤ photo의 길이 ≤ 100
+// 1 ≤ photo[i]의 길이 ≤ 100
+// 3 ≤ photo[i]의 원소(문자열)의 길이 ≤ 7
+// photo[i]의 원소들은 알파벳 소문자로만 이루어져 있습니다.
+// photo[i]의 원소들은 중복된 값이 들어가지 않습니다.
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String[] players = {"mumu", "soe", "poe", "kai", "mine"};
-        String[] callings = {"kai", "kai", "mine", "mine"};
+        String[] name = {"may", "kein", "kain", "radi"};
+        int[] yearning = {5, 10, 1, 3};
+        String[][] photo = {{"may", "kein", "kain", "radi"}, {"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
 
-        solution(players, callings);
+        solution(name, yearning, photo);
 
     }
 
-    public static String[] solution(String[] players, String[] callings) {
+    public static int[] solution(String[] name, int[] yearning, String[][] photo) {
 
-        Map<String, Integer> order = new HashMap<>();
+        Map<String, Integer> pointMap = new HashMap<>();    // 이름과 추억 점수를 매핑할 맵
+        int[] result = new int[photo.length];                // 각 사진의 추억 점수를 담을 배열
 
-        for(int i = 0; i < players.length; i ++) {
-            order.put(players[i], i);
+        // 각 이름에 따른 점수 매핑
+        for(int i = 0; i < name.length; i ++) {
+            pointMap.put(name[i], yearning[i]);
         }
 
-        for(int i = 0; i < callings.length; i ++) {
-            int index = order.get(callings[i]);
-            String oldPlayer = players[index-1];
-            String newPlayer = players[index];
-
-            order.put(players[index], index-1);
-            order.put(players[index-1], index);
-
-            players[index] = oldPlayer;
-            players[index-1] = newPlayer;
+        // 각 사진의 추억 점수 산정
+        for(int i = 0; i < photo.length; i ++) {
+            int point = 0;
+            for(int j = 0; j < photo[i].length; j ++) {
+                String tmp = photo[i][j];
+                if(pointMap.get(tmp) != null) {
+                    point += pointMap.get(tmp);
+                }
+            }
+            result[i] = point;
         }
 
-        for(int i = 0; i < players.length; i ++) {
-            System.out.println(players[i]);
-        }
-
-        String[] answer = players;
+        int[] answer = result;
         return answer;
     }
 
