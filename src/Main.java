52357dbd01f793cf9c4f5b7db8baaf38525b0dc7
soonpyo21 <tnsvy2004@gmail.com>
2024-03-12 23:55:58 +0900

    프로그래머스 - 개인정보 수집 유효기간(1)

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,117 @@
 import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 // 문제
-// 두 문자열 s와 skip, 그리고 자연수 index가 주어질 때, 다음 규칙에 따라 문자열을 만들려 합니다. 암호의 규칙은 다음과 같습니다.
-// 문자열 s의 각 알파벳을 index만큼 뒤의 알파벳으로 바꿔줍니다.
-// index만큼의 뒤의 알파벳이 z를 넘어갈 경우 다시 a로 돌아갑니다.
-// skip에 있는 알파벳은 제외하고 건너뜁니다.
-// 예를 들어 s = "aukks", skip = "wbqd", index = 5일 때,
-// a에서 5만큼 뒤에 있는 알파벳은 f지만 [b, c, d, e, f]에서 'b'와 'd'는 skip에 포함되므로 세지 않습니다.
-// 따라서 'b', 'd'를 제외하고 'a'에서 5만큼 뒤에 있는 알파벳은 [c, e, f, g, h] 순서에 의해 'h'가 됩니다.
-// 나머지 "ukks" 또한 위 규칙대로 바꾸면 "appy"가 되며 결과는 "happy"가 됩니다.
-// 두 문자열 s와 skip, 그리고 자연수 index가 매개변수로 주어질 때 위 규칙대로 s를 변환한 결과를 return하도록 solution 함수를 완성해주세요.
-
+// 고객의 약관 동의를 얻어서 수집된 1~n번으로 분류되는 개인정보 n개가 있습니다.
+// 약관 종류는 여러 가지 있으며 각 약관마다 개인정보 보관 유효기간이 정해져 있습니다.
+// 당신은 각 개인정보가 어떤 약관으로 수집됐는지 알고 있습니다. 수집된 개인정보는 유효기간 전까지만 보관 가능하며,
+// 유효기간이 지났다면 반드시 파기해야 합니다.
+//
+// 예를 들어, A라는 약관의 유효기간이 12 달이고,
+// 2021년 1월 5일에 수집된 개인정보가 A약관으로 수집되었다면 해당 개인정보는 2022년 1월 4일까지 보관 가능하며
+// 2022년 1월 5일부터 파기해야 할 개인정보입니다.
+// 당신은 오늘 날짜로 파기해야 할 개인정보 번호들을 구하려 합니다.
+//
+// 모든 달은 28일까지 있다고 가정합니다.
+//
+// 다음은 오늘 날짜가 2022.05.19일 때의 예시입니다.
+//
+// 약관 종류	유효기간
+// A 	    6 달
+// B	    12 달
+// C	    3 달
+// 번호	개인정보 수집 일자	약관 종류
+// 1	2021.05.02	    A
+// 2	2021.07.01	    B
+// 3	2022.02.19	    C
+// 4	2022.02.20	    C
+//
+// 첫 번째 개인정보는 A약관에 의해 2021년 11월 1일까지 보관 가능하며, 유효기간이 지났으므로 파기해야 할 개인정보입니다.
+// 두 번째 개인정보는 B약관에 의해 2022년 6월 28일까지 보관 가능하며, 유효기간이 지나지 않았으므로 아직 보관 가능합니다.
+// 세 번째 개인정보는 C약관에 의해 2022년 5월 18일까지 보관 가능하며, 유효기간이 지났으므로 파기해야 할 개인정보입니다.
+// 네 번째 개인정보는 C약관에 의해 2022년 5월 19일까지 보관 가능하며, 유효기간이 지나지 않았으므로 아직 보관 가능합니다.
+// 따라서 파기해야 할 개인정보 번호는 [1, 3]입니다.
+//
+// 오늘 날짜를 의미하는 문자열 today,
+// 약관의 유효기간을 담은 1차원 문자열 배열 terms와 수집된 개인정보의 정보를 담은 1차원 문자열 배열 privacies가 매개변수로 주어집니다.
+// 이때 파기해야 할 개인정보의 번호를 오름차순으로 1차원 정수 배열에 담아 return 하도록 solution 함수를 완성해 주세요.
+//
 // 제한사항
-// 5 ≤ s의 길이 ≤ 50
-// 1 ≤ skip의 길이 ≤ 10
-// s와 skip은 알파벳 소문자로만 이루어져 있습니다.
-// skip에 포함되는 알파벳은 s에 포함되지 않습니다.
-// 1 ≤ index ≤ 20
+// today는 "YYYY.MM.DD" 형태로 오늘 날짜를 나타냅니다.
+// 1 ≤ terms의 길이 ≤ 20
+// terms의 원소는 "약관 종류 유효기간" 형태의 약관 종류와 유효기간을 공백 하나로 구분한 문자열입니다.
+// 약관 종류는 A~Z중 알파벳 대문자 하나이며, terms 배열에서 약관 종류는 중복되지 않습니다.
+// 유효기간은 개인정보를 보관할 수 있는 달 수를 나타내는 정수이며, 1 이상 100 이하입니다.
+// 1 ≤ privacies의 길이 ≤ 100
+// privacies[i]는 i+1번 개인정보의 수집 일자와 약관 종류를 나타냅니다.
+// privacies의 원소는 "날짜 약관 종류" 형태의 날짜와 약관 종류를 공백 하나로 구분한 문자열입니다.
+// 날짜는 "YYYY.MM.DD" 형태의 개인정보가 수집된 날짜를 나타내며, today 이전의 날짜만 주어집니다.
+// privacies의 약관 종류는 항상 terms에 나타난 약관 종류만 주어집니다.
+// today와 privacies에 등장하는 날짜의 YYYY는 연도, MM은 월, DD는 일을 나타내며 점(.) 하나로 구분되어 있습니다.
+// 2000 ≤ YYYY ≤ 2022
+// 1 ≤ MM ≤ 12
+// MM이 한 자릿수인 경우 앞에 0이 붙습니다.
+// 1 ≤ DD ≤ 28
+// DD가 한 자릿수인 경우 앞에 0이 붙습니다.
+// 파기해야 할 개인정보가 하나 이상 존재하는 입력만 주어집니다.
 
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
-//        String s = "aukks";
-//        String skip = "wbqd";
-//        int index = 5;
+        String today = "2022.05.19";
+        String[] terms = {"A 6", "B 12", "C 3"};
+        String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
 
-//        String s = "a";
-//        String skip = "cb";
-//        int index = 2;
-
-        String s = "z";
-        String skip = "abcdefghij";
-        int index = 20;
-
-        solution(s, skip, index);
+        solution(today, terms, privacies);
     }
 
-    public static String solution(String s, String skip, int index) {
+    public static int[] solution(String today, String[] terms, String[] privacies) throws ParseException {
 
-        String result = "";
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd");
+        Date date = formatter.parse(today);
 
-        for(int i = 0; i < s.length(); i ++) {
-            char ch = s.charAt(i);
-            int changeIndex = index;
-            for(int j = 0; j < changeIndex; j ++) {
-                ch ++;
-                if(ch > 122) {
-                    ch = (char) (ch - 26);
-                }
-                if(skip.contains(String.valueOf(ch))) {
-                    changeIndex ++;
-                }
-            }
-            result += String.valueOf(ch);
+        Calendar calendar = Calendar.getInstance();
+        Calendar calendar2 = Calendar.getInstance();
+
+        calendar.setTime(date);
+
+        Map<String, Integer> periodMap = new HashMap();
+
+        int[] iArr = new int[privacies.length];
+
+        for(int i = 0; i < terms.length; i ++) {
+            periodMap.put(terms[i].split(" ")[0], Integer.parseInt(terms[i].split(" ")[1]));
         }
 
-        System.out.println(result);
+        for(int i = 0; i < privacies.length; i ++) {
+            String compareDate = privacies[i].split(" ")[0];
+            String type = privacies[i].split(" ")[1];
 
-        String answer = result;
+            int period = periodMap.get(type);
+
+            Date startDate = formatter.parse(compareDate);
+            calendar2.setTime(startDate);
+            calendar2.add(Calendar.MONTH, period);
+
+            if(calendar2.after(calendar)) {
+                System.out.println(calendar.getTime());
+                System.out.println(calendar2.getTime());
+                iArr[i] = i + 1;
+            } else {
+                iArr[i] = 0;
+            }
+        }
+
+        for(int i = 0; i < iArr.length; i ++) {
+            System.out.println(iArr[i]);
+        }
+
+        int[] answer = {};
         return answer;
     }
 }
