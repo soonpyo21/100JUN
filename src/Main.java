56c7a71a764ef386fbc6811e8685fd036af2b83a
soonpyo21 <tnsvy2004@gmail.com>
2024-03-14 23:30:47 +0900

    프로그래머스 - 가장 가까운 같은 글자

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,62 @@
 // 문제
-// 숫자로 이루어진 문자열 t와 p가 주어질 때,
-// t에서 p와 길이가 같은 부분문자열 중에서,
-// 이 부분문자열이 나타내는 수가 p가 나타내는 수보다 작거나 같은 것이 나오는 횟수를 return하는 함수 solution을 완성하세요.
+// 문자열 s가 주어졌을 때, s의 각 위치마다 자신보다 앞에 나왔으면서, 자신과 가장 가까운 곳에 있는 같은 글자가 어디 있는지 알고 싶습니다.
+// 예를 들어, s="banana"라고 할 때,  각 글자들을 왼쪽부터 오른쪽으로 읽어 나가면서 다음과 같이 진행할 수 있습니다.
 //
-// 예를 들어, t="3141592"이고 p="271" 인 경우,
-// t의 길이가 3인 부분 문자열은 314, 141, 415, 159, 592입니다.
-// 이 문자열이 나타내는 수 중 271보다 작거나 같은 수는 141, 159 2개 입니다.
+// b는 처음 나왔기 때문에 자신의 앞에 같은 글자가 없습니다. 이는 -1로 표현합니다.
+// a는 처음 나왔기 때문에 자신의 앞에 같은 글자가 없습니다. 이는 -1로 표현합니다.
+// n은 처음 나왔기 때문에 자신의 앞에 같은 글자가 없습니다. 이는 -1로 표현합니다.
+// a는 자신보다 두 칸 앞에 a가 있습니다. 이는 2로 표현합니다.
+// n도 자신보다 두 칸 앞에 n이 있습니다. 이는 2로 표현합니다.
+// a는 자신보다 두 칸, 네 칸 앞에 a가 있습니다. 이 중 가까운 것은 두 칸 앞이고, 이는 2로 표현합니다.
+// 따라서 최종 결과물은 [-1, -1, -1, 2, 2, 2]가 됩니다.
+//
+// 문자열 s이 주어질 때, 위와 같이 정의된 연산을 수행하는 함수 solution을 완성해주세요.
 //
 // 제한사항
-// 1 ≤ p의 길이 ≤ 18
-// p의 길이 ≤ t의 길이 ≤ 10,000
-// t와 p는 숫자로만 이루어진 문자열이며, 0으로 시작하지 않습니다.
+// 1 ≤ s의 길이 ≤ 10,000
+// s은 영어 소문자로만 이루어져 있습니다.
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 
-//        String t = "3141592";
-//        String p = "271";
+//        String s = "banana";
+//        String s = "foobar";
+        String s = "baaab";
 
-//        String t = "500220839878";
-//        String p = "7";
-
-        String t = "10203";
-        String p = "15";
-
-        solution(t, p);
+        solution(s);
     }
 
-    public static int solution(String t, String p) {
+    public static int[] solution(String s) {
 
-        int len = p.length();
-        long pNum = Long.parseLong(p);
-        int cnt = 0;
+        String copyS = String.valueOf(s.charAt(0));
+        int[] idxList = new int[s.length()];
+        idxList[0] = -1;
+        Map<String, Integer> idxMap = new HashMap<>();
 
-        for(int i = 0; i < t.length(); i ++) {
-            if(!(i + len > t.length())) {
-                long tNum = Long.parseLong(t.substring(i, i + len)) ;
-                if(tNum <= pNum) {
-                    cnt ++;
-                }
+        for(int i = 0; i < s.length(); i ++) {
+            idxMap.put(String.valueOf(s.charAt(i)), 0);
+        }
+
+        for(int i = 1; i < s.length(); i ++) {
+            if(copyS.contains(String.valueOf(s.charAt(i)))) {
+                int idx = s.indexOf(s.charAt(i), idxMap.get(String.valueOf(s.charAt(i))));
+                idxList[i] = i - idx;
+                idxMap.put(String.valueOf(s.charAt(i)), idx + 1);
+            } else {
+                idxList[i] = -1;
+                copyS += s.charAt(i);
             }
         }
 
-        System.out.println(cnt);
+        for(int i : idxList) {
+            System.out.println(i);
+        }
 
-        int answer = cnt;
+        int[] answer = idxList;
         return answer;
     }
 }
