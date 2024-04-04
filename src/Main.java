401c207dab5368de558896b8e5769408db94fa44
soonpyo21 <tnsvy2004@gmail.com>
2024-04-04 23:37:15 +0900

    프로그래머스 - 옹알이 (2) : (1) - 테스트 케이스 1/11/14/16/17 실패

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,41 @@
 // 문제
-// 햄버거 가게에서 일을 하는 상수는 햄버거를 포장하는 일을 합니다.
-// 함께 일을 하는 다른 직원들이 햄버거에 들어갈 재료를 조리해 주면 조리된 순서대로 상수의 앞에 아래서부터 위로 쌓이게 되고,
-// 상수는 순서에 맞게 쌓여서 완성된 햄버거를 따로 옮겨 포장을 하게 됩니다.
-// 상수가 일하는 가게는 정해진 순서(아래서부터, 빵 – 야채 – 고기 - 빵)로 쌓인 햄버거만 포장을 합니다.
-// 상수는 손이 굉장히 빠르기 때문에 상수가 포장하는 동안 속 재료가 추가적으로 들어오는 일은 없으며,
-// 재료의 높이는 무시하여 재료가 높이 쌓여서 일이 힘들어지는 경우는 없습니다.
-//
-// 예를 들어, 상수의 앞에 쌓이는 재료의 순서가 [야채, 빵, 빵, 야채, 고기, 빵, 야채, 고기, 빵]일 때, 상수는 여섯 번째 재료가 쌓였을 때,
-// 세 번째 재료부터 여섯 번째 재료를 이용하여 햄버거를 포장하고,
-// 아홉 번째 재료가 쌓였을 때, 두 번째 재료와 일곱 번째 재료부터 아홉 번째 재료를 이용하여 햄버거를 포장합니다.
-// 즉, 2개의 햄버거를 포장하게 됩니다.
-//
-// 상수에게 전해지는 재료의 정보를 나타내는 정수 배열 ingredient가 주어졌을 때,
-// 상수가 포장하는 햄버거의 개수를 return 하도록 solution 함수를 완성하시오.
+// 머쓱이는 태어난 지 11개월 된 조카를 돌보고 있습니다.
+// 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음과
+// 네 가지 발음을 조합해서 만들 수 있는 발음밖에 하지 못하고 연속해서 같은 발음을 하는 것을 어려워합니다.
+// 문자열 배열 babbling이 매개변수로 주어질 때,
+// 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
 //
 // 제한사항
-// 1 ≤ ingredient의 길이 ≤ 1,000,000
-// ingredient의 원소는 1, 2, 3 중 하나의 값이며, 순서대로 빵, 야채, 고기를 의미합니다.
-
-import java.util.Stack;
+// 1 ≤ babbling의 길이 ≤ 100
+// 1 ≤ babbling[i]의 길이 ≤ 30
+// 문자열은 알파벳 소문자로만 이루어져 있습니다.
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] ingredient = {1, 1, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1};
-
-        solution(ingredient);
+//        String[] babbling = {"aya", "yee", "u", "maa"};
+//        String[] babbling = {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
+        String[] babbling = {"ayayeaya"};
+        solution(babbling);
     }
 
-    public static int solution(int[] ingredient) {
+    public static int solution(String[] babbling) {
 
+        String[] word = {"aya", "ye", "woo", "ma"};
+        String temp = "";
         int result = 0;
-        Stack<Integer> stack = new Stack<>();
 
-        for(int i = 0; i < ingredient.length; i ++) {
-            stack.push(ingredient[i]);
+        for(int i = 0; i < babbling.length; i ++) {
+            temp = babbling[i];
+            for(int j = 0; j < word.length; j ++) {
 
-            if(stack.size() >= 4) {
-                if(stack.get(stack.size()-4) == 1
-                && stack.get(stack.size()-3) == 2
-                && stack.get(stack.size()-2) == 3
-                && stack.get(stack.size()-1) == 1) {
-                    result ++;
-                    stack.pop();
-                    stack.pop();
-                    stack.pop();
-                    stack.pop();
+                if(babbling[i].contains(word[j])) {
+                    temp = temp.replaceFirst(word[j], "");
                 }
             }
+
+            if("".equals(temp)) result++;
         }
 
         System.out.println(result);
