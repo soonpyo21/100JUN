a01cf3b8fa412d2f8a464d23166796d627fbde4a
soonpyo21 <tnsvy2004@gmail.com>
2024-04-15 23:10:09 +0900

    프로그래머스 - 숫자 짝꿍 (1) - 테스트 케이스 11~15 시간 초과

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,89 @@
+temp
+
 // 문제
-// 한국중학교에 다니는 학생들은 각자 정수 번호를 갖고 있습니다.
-// 이 학교 학생 3명의 정수 번호를 더했을 때 0이 되면 3명의 학생은 삼총사라고 합니다.
-// 예를 들어, 5명의 학생이 있고, 각각의 정수 번호가 순서대로 -2, 3, 0, 2, -5일 때,
-// 첫 번째, 세 번째, 네 번째 학생의 정수 번호를 더하면 0이므로 세 학생은 삼총사입니다.
-// 또한, 두 번째, 네 번째, 다섯 번째 학생의 정수 번호를 더해도 0이므로 세 학생도 삼총사입니다.
-// 따라서 이 경우 한국중학교에서는 두 가지 방법으로 삼총사를 만들 수 있습니다.
+// 두 정수 X, Y의 임의의 자리에서 공통으로 나타나는 정수
+// k(0 ≤ k ≤ 9)들을 이용하여 만들 수 있는 가장 큰 정수를 두 수의 짝꿍이라 합니다
+// (단, 공통으로 나타나는 정수 중 서로 짝지을 수 있는 숫자만 사용합니다).
+// X, Y의 짝꿍이 존재하지 않으면, 짝꿍은 -1입니다. X, Y의 짝꿍이 0으로만 구성되어 있다면, 짝꿍은 0입니다.
 //
-// 한국중학교 학생들의 번호를 나타내는 정수 배열 number가 매개변수로 주어질 때,
-// 학생들 중 삼총사를 만들 수 있는 방법의 수를 return 하도록 solution 함수를 완성하세요.
+// 예를 들어, X = 3403이고 Y = 13203이라면,
+// X와 Y의 짝꿍은 X와 Y에서 공통으로 나타나는 3, 0, 3으로 만들 수 있는 가장 큰 정수인 330입니다.
+// 다른 예시로 X = 5525이고 Y = 1255이면
+// X와 Y의 짝꿍은 X와 Y에서 공통으로 나타나는 2, 5, 5로 만들 수 있는 가장 큰 정수인 552입니다
+// (X에는 5가 3개, Y에는 5가 2개 나타나므로 남는 5 한 개는 짝 지을 수 없습니다.)
+//
+// 두 정수 X, Y가 주어졌을 때, X, Y의 짝꿍을 return하는 solution 함수를 완성해주세요.
 //
 // 제한사항
-// 3 ≤ number의 길이 ≤ 13
-// -1,000 ≤ number의 각 원소 ≤ 1,000
-// 서로 다른 학생의 정수 번호가 같을 수 있습니다.
+// 3 ≤ X, Y의 길이(자릿수) ≤ 3,000,000입니다.
+// X, Y는 0으로 시작하지 않습니다.
+// X, Y의 짝꿍은 상당히 큰 정수일 수 있으므로, 문자열로 반환합니다.
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-//        int[] number = {-2, 3, 0, 2, -5};
-        int[] number = {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0};
+//        String X = "100";
+//        String Y = "2345";
 
-        solution(number);
+//        String X = "100";
+//        String Y = "203045";
+
+        String X = "12321";
+        String Y = "42531";
+
+        solution(X, Y);
 
     }
 
-    public static int solution(int[] number) {
+    public static String solution(String X, String Y) {
 
-        Set<String> set = new HashSet<>();
+        String result = "";
+        List<Integer> iList = new ArrayList<>();
+        List<String> xList = new ArrayList<>();
+        List<String> yList = new ArrayList<>();
 
-        for(int i = 0; i < number.length; i ++) {
-            int first = number[i];
-            for(int j = 0; j < number.length; j ++) {
-                if(i == j) continue;
-                int second = number[j];
-                for(int k = 0; k < number.length; k ++) {
-                    if(k == i || k == j) continue;
-                    int third = number[k];
-                    if(first + second + third == 0) {
-                        Integer[] iArr = {i, j, k};
-                        Arrays.sort(iArr);
-                        set.add("" + iArr[0] + iArr[1] + iArr[2]);
-                        System.out.println(first + "" + second + "" + third);
-                    }
+        for(int i = 0; i < X.length(); i++) {
+            xList.add(String.valueOf(X.charAt(i)));
+        }
+
+        for(int i = 0; i < Y.length(); i++) {
+            yList.add(String.valueOf(Y.charAt(i)));
+        }
+
+        for(int i = xList.size()-1; i >= 0; i --) {
+            for(int j = yList.size()-1; j >= 0; j --) {
+                if(xList.get(i).equals(yList.get(j))) {
+                    iList.add(Integer.parseInt(xList.get(i)));
+                    xList.remove(i);
+                    yList.remove(j);
+                    break;
                 }
             }
         }
 
-        System.out.println(set.size());
+        Collections.sort(iList, Collections.reverseOrder());
 
-        int answer = 0;
+        if(iList.size() == 0) {
+            result = "-1";
+        } else {
+            if(iList.get(0) == 0) {
+                result = "0";
+            } else {
+                for(Integer i : iList) {
+                    result += i + "";
+                }
+            }
+        }
+
+        System.out.println(result);
+
+        String answer = result;
         return answer;
     }
 }
+
