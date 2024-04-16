a53fd14bd5fcdfbd66e2bc67b78b233faef9d3ea
soonpyo21 <tnsvy2004@gmail.com>
2024-04-16 23:49:49 +0900

    프로그래머스 - 최소직사각형 - 넓이와 높이 영역을 구분 후 최대값 탐색

--- src/Main.java
+++ src/Main.java
@@ -1,85 +1,55 @@
-temp
-
 // 문제
-// 두 정수 X, Y의 임의의 자리에서 공통으로 나타나는 정수
-// k(0 ≤ k ≤ 9)들을 이용하여 만들 수 있는 가장 큰 정수를 두 수의 짝꿍이라 합니다
-// (단, 공통으로 나타나는 정수 중 서로 짝지을 수 있는 숫자만 사용합니다).
-// X, Y의 짝꿍이 존재하지 않으면, 짝꿍은 -1입니다. X, Y의 짝꿍이 0으로만 구성되어 있다면, 짝꿍은 0입니다.
+// 명함 지갑을 만드는 회사에서 지갑의 크기를 정하려고 합니다.
+// 다양한 모양과 크기의 명함들을 모두 수납할 수 있으면서, 작아서 들고 다니기 편한 지갑을 만들어야 합니다.
+// 이러한 요건을 만족하는 지갑을 만들기 위해 디자인팀은 모든 명함의 가로 길이와 세로 길이를 조사했습니다.
+// 아래 표는 4가지 명함의 가로 길이와 세로 길이를 나타냅니다.
 //
-// 예를 들어, X = 3403이고 Y = 13203이라면,
-// X와 Y의 짝꿍은 X와 Y에서 공통으로 나타나는 3, 0, 3으로 만들 수 있는 가장 큰 정수인 330입니다.
-// 다른 예시로 X = 5525이고 Y = 1255이면
-// X와 Y의 짝꿍은 X와 Y에서 공통으로 나타나는 2, 5, 5로 만들 수 있는 가장 큰 정수인 552입니다
-// (X에는 5가 3개, Y에는 5가 2개 나타나므로 남는 5 한 개는 짝 지을 수 없습니다.)
+// 명함 번호	가로 길이	세로 길이
+// 1	    60	    50
+// 2	    30	    70
+// 3	    60	    30
+// 4	    80	    40
 //
-// 두 정수 X, Y가 주어졌을 때, X, Y의 짝꿍을 return하는 solution 함수를 완성해주세요.
+// 가장 긴 가로 길이와 세로 길이가 각각 80, 70이기 때문에 80(가로) x 70(세로) 크기의 지갑을 만들면 모든 명함들을 수납할 수 있습니다.
+// 하지만 2번 명함을 가로로 눕혀 수납한다면 80(가로) x 50(세로) 크기의 지갑으로 모든 명함들을 수납할 수 있습니다.
+// 이때의 지갑 크기는 4000(=80 x 50)입니다.
+//
+// 모든 명함의 가로 길이와 세로 길이를 나타내는 2차원 배열 sizes가 매개변수로 주어집니다.
+// 모든 명함을 수납할 수 있는 가장 작은 지갑을 만들 때, 지갑의 크기를 return 하도록 solution 함수를 완성해주세요.
 //
 // 제한사항
-// 3 ≤ X, Y의 길이(자릿수) ≤ 3,000,000입니다.
-// X, Y는 0으로 시작하지 않습니다.
-// X, Y의 짝꿍은 상당히 큰 정수일 수 있으므로, 문자열로 반환합니다.
-
-import java.util.Arrays;
+// sizes의 길이는 1 이상 10,000 이하입니다.
+// sizes의 원소는 [w, h] 형식입니다.
+// w는 명함의 가로 길이를 나타냅니다.
+// h는 명함의 세로 길이를 나타냅니다.
+// w와 h는 1 이상 1,000 이하인 자연수입니다.
 
 public class Main {
 
     public static void main(String[] args) {
 
-//        String X = "100";
-//        String Y = "2345";
+        int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
 
-//        String X = "100";
-//        String Y = "203045";
-
-        String X = "12321";
-        String Y = "42531";
-
-        solution(X, Y);
+        solution(sizes);
 
     }
 
-    public static String solution(String X, String Y) {
+    public static int solution(int[][] sizes) {
 
-        StringBuffer sb = new StringBuffer();
-        String result = "";
+        int w = 0;
+        int h = 0;
+        int max = 0;
+        int min = 0;
 
-        String[] xSplit = X.split("");
-        String[] ySplit = Y.split("");
+        for(int i = 0; i < sizes.length; i ++) {
+            max = Math.max(sizes[i][0], sizes[i][1]);
+            min = Math.min(sizes[i][0], sizes[i][1]);
 
-        int[] xArr = new int[10];
-        int[] yArr = new int[10];
-
-        Arrays.fill(xArr, 0);
-        Arrays.fill(yArr, 0);
-
-        for(int i = 0; i < X.length(); i ++) {
-            xArr[Integer.parseInt(xSplit[i])] = ++ xArr[Integer.parseInt(xSplit[i])];
+            if(max > w) w = max;
+            if(min > h) h = min;
         }
 
-        for(int i = 0; i < Y.length(); i ++) {
-            yArr[Integer.parseInt(ySplit[i])] = ++ yArr[Integer.parseInt(ySplit[i])];
-        }
-
-        for(int i = 9; i >= 0; i --) {
-            if(xArr[i] > 0 && yArr[i] > 0) {
-                int loop = xArr[i] > yArr[i] ? yArr[i] : xArr[i];
-                for(int j = 0; j < loop; j ++) {
-                    sb.append(i);
-                }
-            }
-        }
-
-        if(sb.length() == 0) {
-            result = "-1";
-        } else {
-            if(sb.charAt(0) == '0') {
-                result = "0";
-            } else {
-                result = sb.toString();
-            }
-        }
-
-        String answer = result;
+        int answer = w * h;
         return answer;
     }
 }
