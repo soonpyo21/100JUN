d146706979d9db9d532c2309b07c6cd0424d1d59
soonpyo21 <tnsvy2004@gmail.com>
2024-04-17 22:39:14 +0900

    프로그래머스 - 없는 숫자 더하기

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,32 @@
 // 문제
-// 명함 지갑을 만드는 회사에서 지갑의 크기를 정하려고 합니다.
-// 다양한 모양과 크기의 명함들을 모두 수납할 수 있으면서, 작아서 들고 다니기 편한 지갑을 만들어야 합니다.
-// 이러한 요건을 만족하는 지갑을 만들기 위해 디자인팀은 모든 명함의 가로 길이와 세로 길이를 조사했습니다.
-// 아래 표는 4가지 명함의 가로 길이와 세로 길이를 나타냅니다.
-//
-// 명함 번호	가로 길이	세로 길이
-// 1	    60	    50
-// 2	    30	    70
-// 3	    60	    30
-// 4	    80	    40
-//
-// 가장 긴 가로 길이와 세로 길이가 각각 80, 70이기 때문에 80(가로) x 70(세로) 크기의 지갑을 만들면 모든 명함들을 수납할 수 있습니다.
-// 하지만 2번 명함을 가로로 눕혀 수납한다면 80(가로) x 50(세로) 크기의 지갑으로 모든 명함들을 수납할 수 있습니다.
-// 이때의 지갑 크기는 4000(=80 x 50)입니다.
-//
-// 모든 명함의 가로 길이와 세로 길이를 나타내는 2차원 배열 sizes가 매개변수로 주어집니다.
-// 모든 명함을 수납할 수 있는 가장 작은 지갑을 만들 때, 지갑의 크기를 return 하도록 solution 함수를 완성해주세요.
+// 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다.
+// numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
 //
 // 제한사항
-// sizes의 길이는 1 이상 10,000 이하입니다.
-// sizes의 원소는 [w, h] 형식입니다.
-// w는 명함의 가로 길이를 나타냅니다.
-// h는 명함의 세로 길이를 나타냅니다.
-// w와 h는 1 이상 1,000 이하인 자연수입니다.
+// 1 ≤ numbers의 길이 ≤ 9
+// 0 ≤ numbers의 모든 원소 ≤ 9
+// numbers의 모든 원소는 서로 다릅니다.
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+        int[] numbers = {1,2,3,4,6,7,8,0};
 
-        solution(sizes);
+        solution(numbers);
 
     }
 
-    public static int solution(int[][] sizes) {
+    public static int solution(int[] numbers) {
 
-        int w = 0;
-        int h = 0;
-        int max = 0;
-        int min = 0;
-
-        for(int i = 0; i < sizes.length; i ++) {
-            max = Math.max(sizes[i][0], sizes[i][1]);
-            min = Math.min(sizes[i][0], sizes[i][1]);
-
-            if(max > w) w = max;
-            if(min > h) h = min;
+        int sum = 0;
+        for(int i : numbers) {
+            sum += i;
         }
 
-        int answer = w * h;
+        System.out.println(45 - sum);
+
+        int answer = 45 - sum;
         return answer;
     }
 }
