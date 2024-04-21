4250da518d24edcf1b6ee7147dc71cd5e1776ee4
soonpyo21 <tnsvy2004@gmail.com>
2024-04-21 23:39:25 +0900

    프로그래머스 - 약수의 개수와 덧셈

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,48 @@
 // 문제
-// 네오와 프로도가 숫자놀이를 하고 있습니다.
-// 네오가 프로도에게 숫자를 건넬 때 일부 자릿수를 영단어로 바꾼 카드를 건네주면 프로도는 원래 숫자를 찾는 게임입니다.
-//
-// 다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다.
-//
-// 1478 → "one4seveneight"
-// 234567 → "23four5six7"
-// 10203 → "1zerotwozero3"
-// 이렇게 숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어집니다.
-// s가 의미하는 원래 숫자를 return 하도록 solution 함수를 완성해주세요.
-//
-// 참고로 각 숫자에 대응되는 영단어는 다음 표와 같습니다.
-//
-// 숫자	영단어
-// 0	zero
-// 1	one
-// 2	two
-// 3	three
-// 4	four
-// 5	five
-// 6	six
-// 7	seven
-// 8	eight
-// 9	nine
+// 두 정수 left와 right가 매개변수로 주어집니다.
+// left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고,
+// 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
 //
 // 제한사항
-// 1 ≤ s의 길이 ≤ 50
-// s가 "zero" 또는 "0"으로 시작하는 경우는 주어지지 않습니다.
-// return 값이 1 이상 2,000,000,000 이하의 정수가 되는 올바른 입력만 s로 주어집니다.
+// 1 ≤ left ≤ right ≤ 1,000
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String s = "one4seveneight";
+        int left = 13;
+        int right = 17;
 
-        solution(s);
+        solution(left, right);
 
     }
 
-    public static int solution(String s) {
+    public static int solution(int left, int right) {
 
-        String replaceS = s.replace("zero", "0")
-                            .replace("one","1")
-                            .replace("two","2")
-                            .replace("three","3")
-                            .replace("four","4")
-                            .replace("five","5")
-                            .replace("six","6")
-                            .replace("seven","7")
-                            .replace("eight","8")
-                            .replace("nine","9");
+        int result = 0;
 
-        int answer = Integer.parseInt(replaceS);
+        for(int i = left; i <= right; i ++) {
+            int cnt = 0;
+            int sqrt = (int) Math.sqrt(i);
+
+            for(int j = 1; j <= sqrt; j ++) {
+                if(i % j == 0) {
+                    cnt ++;
+                    if(i / j != j) {
+                        cnt ++;
+                    }
+                }
+            }
+
+            if(cnt % 2 == 0) {
+                result += i;
+            } else {
+                result -= i;
+            }
+
+        }
+
+        int answer = result;
         return answer;
     }
 }
