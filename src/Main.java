0f2e3178d7083da0aeaab892a6f00a914e86b434
soonpyo21 <tnsvy2004@gmail.com>
2024-04-22 21:42:18 +0900

    프로그래머스 - 음양 더하기

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,31 @@
 // 문제
-// 두 정수 left와 right가 매개변수로 주어집니다.
-// left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고,
-// 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
+// 어떤 정수들이 있습니다.
+// 이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다.
+// 실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
 //
 // 제한사항
-// 1 ≤ left ≤ right ≤ 1,000
+// absolutes의 길이는 1 이상 1,000 이하입니다.
+// absolutes의 모든 수는 각각 1 이상 1,000 이하입니다.
+// signs의 길이는 absolutes의 길이와 같습니다.
+// signs[i] 가 참이면 absolutes[i] 의 실제 정수가 양수임을, 그렇지 않으면 음수임을 의미합니다.
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int left = 13;
-        int right = 17;
+        int[] absolutes = {4,7,12};
+        boolean[] signs = {true,false,true};
 
-        solution(left, right);
+        solution(absolutes, signs);
 
     }
 
-    public static int solution(int left, int right) {
+    public static int solution(int[] absolutes, boolean[] signs) {
 
         int result = 0;
 
-        for(int i = left; i <= right; i ++) {
-            int cnt = 0;
-            int sqrt = (int) Math.sqrt(i);
-
-            for(int j = 1; j <= sqrt; j ++) {
-                if(i % j == 0) {
-                    cnt ++;
-                    if(i / j != j) {
-                        cnt ++;
-                    }
-                }
-            }
-
-            if(cnt % 2 == 0) {
-                result += i;
-            } else {
-                result -= i;
-            }
-
+        for(int i = 0; i < absolutes.length; i ++) {
+            result = signs[i] ? result + absolutes[i] : result - absolutes[i];
         }
 
         int answer = result;
