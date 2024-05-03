5c9b54896f228cf1f708b76b3b3d842d928b1dbd
soonpyo21 <tnsvy2004@gmail.com>
2024-05-04 00:02:23 +0900

    프로그래머스 - x만큼 간격이 있는 n개의 숫자

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,32 @@
 // 문제
-// 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
-// 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+// 함수 solution은 정수 x와 자연수 n을 입력 받아,
+// x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다.
+// 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
 //
 // 제한사항
-// n과 m은 각각 1000 이하인 자연수입니다.
-
-import java.util.Scanner;
+// x는 -10000000 이상, 10000000 이하인 정수입니다.
+// n은 1000 이하인 자연수입니다.
 
 public class Main {
 
     public static void main(String[] args) {
 
-        solution();
+        int x = 2;
+        int n = 5;
+
+        solution(x,n);
     }
 
-    public static void solution() {
+    public static long[] solution(int x, int n) {
 
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        long[] answer = new long[n];
+        answer[0] = x;
 
-        for(int i = 0; i < b; i ++) {
-            System.out.println("*".repeat(a));
+        for(int i = 1; i < n; i ++) {
+            answer[i] = answer[i - 1] + x;
         }
+
+        return answer;
     }
 }
 
