9db394ce6d347de69e8bb8bde790cf081996577d
soonpyo21 <tnsvy2004@gmail.com>
2024-05-03 23:58:24 +0900

    프로그래머스 - 직사각형 별찍기

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,28 @@
 // 문제
-// 정수 배열 numbers가 주어집니다.
-// numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를
-// 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+// 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+// 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
 //
 // 제한사항
-// numbers의 길이는 2 이상 100 이하입니다.
-// numbers의 모든 수는 0 이상 100 이하입니다.
+// n과 m은 각각 1000 이하인 자연수입니다.
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] numbers = {2,1,3,4,1};
-
-        solution(numbers);
+        solution();
     }
 
-    public static int[] solution(int[] numbers) {
+    public static void solution() {
 
-        Set<Integer> set = new HashSet<>();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        for(int i = 0; i < numbers.length; i ++) {
-            for(int j = 0; j < numbers.length; j ++) {
-                if(i != j) {
-                    set.add(numbers[i] + numbers[j]);
-                }
-            }
+        for(int i = 0; i < b; i ++) {
+            System.out.println("*".repeat(a));
         }
-
-        int[] result = new int[set.size()];
-        int idx = 0;
-        Iterator<Integer> iterSet = set.iterator();
-        while(iterSet.hasNext()) {
-            result[idx] = iterSet.next();
-            idx ++;
-        }
-
-        Arrays.sort(result);
-
-        int[] answer = result;
-        return answer;
     }
 }
 
