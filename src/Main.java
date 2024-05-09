cfe3c3b680282195aba381caa7be6bf717d6fce3
soonpyo21 <tnsvy2004@gmail.com>
2024-05-10 00:02:32 +0900

    프로그래머스 - 하샤드 수

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,30 @@
 // 문제
-// 프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
-// 전화번호가 문자열 phone_number로 주어졌을 때,
-// 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
+// 양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다.
+// 예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다.
+// 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
 //
 // 제한사항
-// phone_number는 길이 4 이상, 20이하인 문자열입니다.
+// x는 1 이상, 10000 이하인 정수입니다.
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String phone_number = "01033334444";
+        int x = 10;
 
-        solution(phone_number);
+        solution(x);
     }
 
-    public static String solution(String phone_number) {
+    public static boolean solution(int x) {
 
-        int len = phone_number.length();
-        String answer = "*".repeat(len - 4) + phone_number.substring(len - 4, len);
+        String str = String.valueOf(x);
+        int sum = 0;
+
+        for(int i = 0; i < str.length(); i ++) {
+            sum += (int) str.charAt(i) - '0';
+        }
+
+        boolean answer = x % sum == 0 ? true : false;
         return answer;
     }
 }
