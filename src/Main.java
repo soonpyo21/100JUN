c0b2eff7f3126d4565c8b9272843ae9134762504
soonpyo21 <tnsvy2004@gmail.com>
2024-05-11 23:52:50 +0900

    프로그래머스 - 평균 구하기

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,27 @@
 // 문제
-// 양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다.
-// 예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다.
-// 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
+// 정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
 //
 // 제한사항
-// x는 1 이상, 10000 이하인 정수입니다.
+// arr은 길이 1 이상, 100 이하인 배열입니다.
+// arr의 원소는 -10,000 이상 10,000 이하인 정수입니다.
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int x = 10;
+        int[] arr = {1,2,3,4};
 
-        solution(x);
+        solution(arr);
     }
 
-    public static boolean solution(int x) {
+    public static double solution(int[] arr) {
 
-        String str = String.valueOf(x);
-        int sum = 0;
-
-        for(int i = 0; i < str.length(); i ++) {
-            sum += (int) str.charAt(i) - '0';
+        double answer = 0;
+        for(int i : arr) {
+            answer += i;
         }
 
-        boolean answer = x % sum == 0 ? true : false;
-        return answer;
+        return answer / arr.length;
     }
 }
 
