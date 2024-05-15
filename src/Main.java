85d351dc9f6673979b73002180bab90be26cfe3a
soonpyo21 <tnsvy2004@gmail.com>
2024-05-16 00:01:41 +0900

    프로그래머스 - 정수 제곱근 판별

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,24 @@
 // 문제
-// 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수,
-// solution을 완성해주세요.
-// 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요.
-// 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+// 임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+// n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고,
+// n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
 //
 // 제한사항
-// arr은 길이 1 이상인 배열입니다.
-// 인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+// n은 1이상, 50000000000000 이하인 양의 정수입니다.
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] arr = {4,3,2,1};
+        long n = 121;
 
-        solution(arr);
+        solution(n);
     }
 
-    public static int[] solution(int[] arr) {
+    public static long solution(long n) {
 
-        int length = arr.length == 1 ? 1 : arr.length - 1;
-        int[] answer = new int[length];
-        int min = arr[0];
-
-        for(int i : arr) {
-            min = i > min ? min : i;
-        }
-
-        int idx = 0;
-        for(int i : arr) {
-            if(i != min) {
-                answer[idx] = i;
-                idx ++;
-            }
-        }
-
-        if(idx == 0) {
-            answer[0] = -1;
-        }
+        boolean isSqrt = Math.sqrt(n) % 1 == 0.0 ? true : false;
+        long answer = (long) (isSqrt ? Math.pow(Math.sqrt(n) + 1, 2) : -1);
 
         return answer;
     }
