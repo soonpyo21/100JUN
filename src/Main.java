351ef879c9821856eeb1b78bd24b2af178457e9d
soonpyo21 <tnsvy2004@gmail.com>
2024-05-15 23:58:27 +0900

    프로그래머스 - 제일 작은 수 제거하기

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,44 @@
 // 문제
-// 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+// 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수,
+// solution을 완성해주세요.
+// 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요.
+// 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
 //
 // 제한사항
-// num은 int 범위의 정수입니다.
-// 0은 짝수입니다.
+// arr은 길이 1 이상인 배열입니다.
+// 인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int num = 3;
+        int[] arr = {4,3,2,1};
 
-        solution(num);
+        solution(arr);
     }
 
-    public static String solution(int num) {
+    public static int[] solution(int[] arr) {
 
-        String answer = num % 2 == 0 ? "Even" : "Odd";
+        int length = arr.length == 1 ? 1 : arr.length - 1;
+        int[] answer = new int[length];
+        int min = arr[0];
+
+        for(int i : arr) {
+            min = i > min ? min : i;
+        }
+
+        int idx = 0;
+        for(int i : arr) {
+            if(i != min) {
+                answer[idx] = i;
+                idx ++;
+            }
+        }
+
+        if(idx == 0) {
+            answer[0] = -1;
+        }
+
         return answer;
     }
 }
