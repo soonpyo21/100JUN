841b62cc10d90cc0e2faf4f24571b945bcac513c
soonpyo21 <tnsvy2004@gmail.com>
2024-05-17 00:28:39 +0900

    프로그래머스 - 정수 내림차순으로 배치하기

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,35 @@
 // 문제
-// 임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
-// n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고,
-// n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+// 함수 solution은 정수 n을 매개변수로 입력받습니다.
+// n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요.
+// 예를들어 n이 118372면 873211을 리턴하면 됩니다.
 //
 // 제한사항
-// n은 1이상, 50000000000000 이하인 양의 정수입니다.
+// n은 1이상 8000000000 이하인 자연수입니다.
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        long n = 121;
+        long n = 118372;
 
         solution(n);
     }
 
     public static long solution(long n) {
 
-        boolean isSqrt = Math.sqrt(n) % 1 == 0.0 ? true : false;
-        long answer = (long) (isSqrt ? Math.pow(Math.sqrt(n) + 1, 2) : -1);
+        String[] sArr = String.valueOf(n).split("");
+        String str = "";
 
+        Arrays.sort(sArr, Collections.reverseOrder());
+
+        for(String s : sArr) {
+            str += s;
+        }
+
+        long answer = Long.parseLong(str);
         return answer;
     }
 }
