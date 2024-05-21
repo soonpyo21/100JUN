f7f0b675b7ef7742c9fd3849a2c147896066bc0d
soonpyo21 <tnsvy2004@gmail.com>
2024-05-22 00:04:50 +0900

    프로그래머스 - 약수의 합

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,26 @@
 // 문제
-// 문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 각 단어는 하나 이상의 공백문자로 구분되어 있습니다.
-// 각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+// 정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
 //
 // 제한사항
-// 문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
-// 첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.
+// n은 0 이상 3000이하인 정수입니다.
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String s = "try hello world";
+        int n = 12;
 
-        solution(s);
+        solution(n);
     }
 
-    public static String solution(String s) {
+    public static int solution(int n) {
 
-        String[] sArr = s.split("");
-        String answer = "";
-        int idx = 0;
+        int answer = 0;
 
-        for(String str : sArr) {
-            if(str.equals(" ")) {
-                answer += " ";
-                idx = 0;
-            } else {
-                answer += idx % 2 == 0 ? str.toUpperCase() : str.toLowerCase();
-                idx ++;
+        for(int i = 1; i <= Math.sqrt(n); i ++) {
+            if(n % i == 0) {
+                answer += i;
+                if(n / i != i) answer += n / i;
             }
         }
 
