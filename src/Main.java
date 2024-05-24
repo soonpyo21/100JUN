8e68910b7104ca7766af735e9dcf23a60cad65eb
soonpyo21 <tnsvy2004@gmail.com>
2024-05-25 00:13:41 +0900

    프로그래머스 - 문자열을 정수로 바꾸기

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,43 @@
 // 문제
-// 어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다.
-// 예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다.
-// "z"는 1만큼 밀면 "a"가 됩니다. 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
+// 문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
 //
 // 제한사항
-// 공백은 아무리 밀어도 공백입니다.
-// s는 알파벳 소문자, 대문자, 공백으로만 이루어져 있습니다.
-// s의 길이는 8000이하입니다.
-// n은 1 이상, 25이하인 자연수입니다.
+// s의 길이는 1 이상 5이하입니다.
+// s의 맨앞에는 부호(+, -)가 올 수 있습니다.
+// s는 부호와 숫자로만 이루어져있습니다.
+// s는 "0"으로 시작하지 않습니다.
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String s = "AB";
-        int n = 1;
+        String s = "-1234";
 
-        solution(s,n);
+        solution(s);
     }
 
-    public static String solution(String s, int n) {
+    public static int solution(String s) {
 
-        String answer = "";
+        int answer = 0;
+        String[] arr = s.split("");
+        boolean sign = true;
+        int x = (int) Math.pow(10, s.length()-1);
 
-        for(int i = 0; i < s.length(); i ++) {
-            char word = s.charAt(i);
-            if(word == ' ') {
-                answer += " ";
+        for(String str : arr) {
+            if(str.equals("-")) {
+                sign = false;
+                x = x / 10;
+                continue;
+            } else if (str.equals("+")) {
+                x = x / 10;
                 continue;
             }
 
-            if(word <= 90) {
-                answer += s.charAt(i) + n <= 90 ? (char) (s.charAt(i) + n) : (char) (s.charAt(i) + n - 26);
-            } else {
-                answer += s.charAt(i) + n <= 122 ? (char) (s.charAt(i) + n) : (char) (s.charAt(i) + n - 26);
-            }
-
+            answer += Integer.parseInt(str) * x;
+            x = x / 10;
         }
 
-        return answer;
+        return answer = sign ? answer : answer * -1;
     }
 }
 
