29fe3a957b9066608a9df7a2681ed215925f3e49
soonpyo21 <tnsvy2004@gmail.com>
2024-05-27 23:25:17 +0900

    프로그래머스 - 수박수박수박수박수박수?

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,27 @@
 // 문제
-// 문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+// 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수,
+// solution을 완성하세요. 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
 //
 // 제한사항
-// s의 길이는 1 이상 5이하입니다.
-// s의 맨앞에는 부호(+, -)가 올 수 있습니다.
-// s는 부호와 숫자로만 이루어져있습니다.
-// s는 "0"으로 시작하지 않습니다.
+// n은 길이 10,000이하인 자연수입니다.
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String s = "-1234";
+        int n = 3;
 
-        solution(s);
+        solution(n);
     }
 
-    public static int solution(String s) {
+    public static String solution(int n) {
+        String answer = "";
 
-        int answer = 0;
-        String[] arr = s.split("");
-        boolean sign = true;
-        int x = (int) Math.pow(10, s.length()-1);
-
-        for(String str : arr) {
-            if(str.equals("-")) {
-                sign = false;
-                x = x / 10;
-                continue;
-            } else if (str.equals("+")) {
-                x = x / 10;
-                continue;
-            }
-
-            answer += Integer.parseInt(str) * x;
-            x = x / 10;
+        for(int i = 0; i < n; i ++) {
+            answer += i % 2 == 0 ? "수" : "박";
         }
 
-        return answer = sign ? answer : answer * -1;
+        return answer;
     }
 }
 
