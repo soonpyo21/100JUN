3c1e577d8b0681400c75dad467fc1d0a408a2db0
soonpyo21 <tnsvy2004@gmail.com>
2024-05-30 21:58:18 +0900

    프로그래머스 - 문자열 다루기 기본

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,35 @@
 // 문제
-// String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아,
-// "김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요.
-// seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+// 문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수,
+// solution을 완성하세요.
+// 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
 //
 // 제한사항
-// seoul은 길이 1 이상, 1000 이하인 배열입니다.
-// seoul의 원소는 길이 1 이상, 20 이하인 문자열입니다.
-// "Kim"은 반드시 seoul 안에 포함되어 있습니다.
+// s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+// s는 영문 알파벳 대소문자 또는 0부터 9까지 숫자로 이루어져 있습니다.
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String[] seoul = {"Jane", "Kim"};
+        String s = "a234";
 
-        solution(seoul);
+        solution(s);
     }
 
-    public static String solution(String[] seoul) {
-        String answer = "";
+    public static boolean solution(String s) {
+        boolean answer = true;
+        char[] arr = s.toCharArray();
 
-        for(int i = 0; i < seoul.length; i ++) {
-            if(seoul[i].equals("Kim")) answer = "김서방은 " + i + "에 있다";
+        if(!(arr.length == 4 || arr.length == 6)) {
+            answer = false;
+            return answer;
+        }
+
+        for(char c : arr) {
+            if(!(c >= '0' && c <= '9')) {
+                answer = false;
+                break;
+            }
         }
 
         return answer;
