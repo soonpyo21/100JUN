79ed558ca6f3f0a0f10700196cf044883cb3ce08
soonpyo21 <tnsvy2004@gmail.com>
2024-06-03 00:01:21 +0900

    프로그래머스 - 문자열 내 마음대로 정렬하기

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,69 @@
 // 문제
-// 대문자와 소문자가 섞여있는 문자열 s가 주어집니다.
-// s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요.
-// 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
-// 예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
+// 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다.
+// 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
 //
 // 제한사항
-// 문자열 s의 길이 : 50 이하의 자연수
-// 문자열 s는 알파벳으로만 이루어져 있습니다.
+// strings는 길이 1 이상, 50이하인 배열입니다.
+// strings의 원소는 소문자 알파벳으로 이루어져 있습니다.
+// strings의 원소는 길이 1 이상, 100이하인 문자열입니다.
+// 모든 strings의 원소의 길이는 n보다 큽니다.
+// 인덱스 1의 문자가 같은 문자열이 여럿 일 경우, 사전순으로 앞선 문자열이 앞쪽에 위치합니다.
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String s = "pPoooyY";
+        String[] strings = {"sun", "bed", "car"};
+        int n = 1;
 
-        solution(s);
+        solution(strings, n);
     }
 
-    public static boolean solution(String s) {
-        char[] arr = s.toCharArray();
-        int cnt1 = 0;
-        int cnt2 = 0;
+    public static String[] solution(String[] strings, int n) {
+        List<Character> cList = new ArrayList<>();
 
-        for(char c : arr) {
-            if(c == 'p' || c == 'P') cnt1 ++;
-            if(c == 'y' || c == 'Y') cnt2 ++;
+        for(int i = 0; i < strings.length; i ++) {
+            cList.add(strings[i].charAt(n));
         }
 
-        boolean answer = cnt1 == cnt2 ? true : false;
+        Collections.sort(cList);
+
+        List<String> sList = new ArrayList<>();
+
+        for(String s : strings) {
+            sList.add(s);
+        }
+
+        String[] answer = new String[sList.size()];
+
+        for(int i = 0; i < cList.size(); i ++) {
+            for(int j = 0; j < sList.size(); j ++) {
+                if(cList.get(i) == sList.get(j).charAt(n)) {
+                    answer[i] = sList.get(j);
+                    sList.remove(j);
+                    break;
+                }
+            }
+        }
+
+        for(int i = 0; i < answer.length; i ++) {
+            for(int j = 0; j < answer.length; j ++) {
+                if(answer[i].charAt(n) == answer[j].charAt(n)) {
+                    int comp = answer[i].compareTo(answer[j]);
+                    if(comp < 0) {
+                        String temp = answer[i];
+                        String temp2 = answer[j];
+                        answer[j] = temp;
+                        answer[i] = temp2;
+                    }
+                }
+            }
+        }
+
         return answer;
     }
 }
