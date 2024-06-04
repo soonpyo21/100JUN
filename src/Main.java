67c5e611bf8fc3bf44f10177cf84d77f258b610c
soonpyo21 <tnsvy2004@gmail.com>
2024-06-04 23:35:11 +0900

    프로그래머스 - 나누어 떨어지는 숫자 배열

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,50 @@
 // 문제
-// 두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
-// 예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+// array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
+// divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
 //
 // 제한사항
-// a와 b가 같은 경우는 둘 중 아무 수나 리턴하세요.
-// a와 b는 -10,000,000 이상 10,000,000 이하인 정수입니다.
-// a와 b의 대소관계는 정해져있지 않습니다.
+// arr은 자연수를 담은 배열입니다.
+// 정수 i, j에 대해 i ≠ j 이면 arr[i] ≠ arr[j] 입니다.
+// divisor는 자연수입니다.
+// array는 길이 1 이상인 배열입니다.
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int a = 3;
-        int b = 5;
+        int[] arr = {5,9,7,10};
+        int divisor = 5;
 
-        solution(a,b);
+        solution(arr,divisor);
     }
 
-    public static long solution(int a, int b) {
-        long answer = 0;
-        for(int i = Math.min(a,b); i <= Math.max(a,b); i ++) {
-            answer += i;
+    public static int[] solution(int[] arr, int divisor) {
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < arr.length; i ++) {
+            if(arr[i] % divisor == 0) list.add(arr[i]);
         }
 
-        return answer;
+        Collections.sort(list);
+
+        if(list.size() == 0) {
+            int[] answer = new int[1];
+            answer[0] = -1;
+
+            return answer;
+        } else {
+            int[] answer = new int[list.size()];
+
+            for(int i = 0; i < list.size(); i ++) {
+                answer[i] = list.get(i);
+            }
+
+            return answer;
+        }
     }
 }
 
