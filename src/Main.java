481cc3cc0c46d2af0cefca3f1c1f6499111544f9
soonpyo21 <tnsvy2004@gmail.com>
2024-06-07 01:50:17 +0900

    프로그래머스 - 가운데 글자 가져오기

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,19 @@
 // 문제
-// 배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다.
-// 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다.
-// 단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다. 예를 들면,
-// arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
-// arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
-// 배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
+// 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
 //
 // 제한사항
-// 배열 arr의 크기 : 1,000,000 이하의 자연수
-// 배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
-
-import java.util.ArrayList;
-import java.util.List;
+// s는 길이가 1 이상, 100이하인 스트링입니다.
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {1,1,3,3,0,1,1};
-        solution(arr);
+        String s = "abcde";
+        solution(s);
     }
 
-    public static int[] solution(int[] arr) {
-        List<Integer> list = new ArrayList<>();
-        list.add(arr[0]);
-
-        for(int i = 1; i < arr.length; i ++) {
-            if(arr[i-1] != arr[i]) list.add(arr[i]);
-        }
-
-        int[] answer = new int[list.size()];
-
-        for(int i = 0; i < list.size(); i ++) {
-            answer[i] = list.get(i);
-        }
+    public static String solution(String s) {
+        int len = s.length();
+        String answer = len % 2 == 0 ? s.substring(len/2-1, len/2+1) : s.substring(len/2, len/2+1);
 
         return answer;
     }
