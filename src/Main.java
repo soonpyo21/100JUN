f53123ff496a169b9c1c422e910473553aa125d3
soonpyo21 <tnsvy2004@gmail.com>
2024-06-06 23:45:30 +0900

    프로그래머스 - 같은 숫자는 싫어

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,40 @@
 // 문제
-// array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
-// divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
+// 배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다.
+// 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다.
+// 단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다. 예를 들면,
+// arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
+// arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
+// 배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
 //
 // 제한사항
-// arr은 자연수를 담은 배열입니다.
-// 정수 i, j에 대해 i ≠ j 이면 arr[i] ≠ arr[j] 입니다.
-// divisor는 자연수입니다.
-// array는 길이 1 이상인 배열입니다.
+// 배열 arr의 크기 : 1,000,000 이하의 자연수
+// 배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        int[] arr = {5,9,7,10};
-        int divisor = 5;
-
-        solution(arr,divisor);
+        int[] arr = {1,1,3,3,0,1,1};
+        solution(arr);
     }
 
-    public static int[] solution(int[] arr, int divisor) {
+    public static int[] solution(int[] arr) {
         List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
 
-        for(int i = 0; i < arr.length; i ++) {
-            if(arr[i] % divisor == 0) list.add(arr[i]);
+        for(int i = 1; i < arr.length; i ++) {
+            if(arr[i-1] != arr[i]) list.add(arr[i]);
         }
 
-        Collections.sort(list);
+        int[] answer = new int[list.size()];
 
-        if(list.size() == 0) {
-            int[] answer = new int[1];
-            answer[0] = -1;
-
-            return answer;
-        } else {
-            int[] answer = new int[list.size()];
-
-            for(int i = 0; i < list.size(); i ++) {
-                answer[i] = list.get(i);
-            }
-
-            return answer;
+        for(int i = 0; i < list.size(); i ++) {
+            answer[i] = list.get(i);
         }
+
+        return answer;
     }
 }
 
