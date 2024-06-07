d6c88edf37c6a8c3e95ad29b0ae8e778f4da743d
soonpyo21 <tnsvy2004@gmail.com>
2024-06-08 00:11:38 +0900

    프로그래머스 - 2016년

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,30 @@
 // 문제
-// 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+// 2016년 1월 1일은 금요일입니다.
+// 2016년 a월 b일은 무슨 요일일까요?
+// 두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요.
+// 요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT입니다.
+// 예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 "TUE"를 반환하세요.
 //
 // 제한사항
-// s는 길이가 1 이상, 100이하인 스트링입니다.
+// 2016년은 윤년입니다.
+// 2016년 a월 b일은 실제로 있는 날입니다. (13월 26일이나 2월 45일같은 날짜는 주어지지 않습니다)
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 public class Main {
 
     public static void main(String[] args) {
-        String s = "abcde";
-        solution(s);
+        int a = 5;
+        int b = 24;
+        solution(a,b);
     }
 
-    public static String solution(String s) {
-        int len = s.length();
-        String answer = len % 2 == 0 ? s.substring(len/2-1, len/2+1) : s.substring(len/2, len/2+1);
+    public static String solution(int a, int b) {
+        LocalDate date = LocalDate.of(2016, a, b);
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
 
+        String answer = dayOfWeek.toString().substring(0,3);
         return answer;
     }
 }
