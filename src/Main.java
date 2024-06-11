1eac15479db2e7c2426627f0441ffc59806bf3da
soonpyo21 <tnsvy2004@gmail.com>
2024-06-12 00:04:38 +0900

    프로그래머스 - 예산

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,41 @@
 // 문제
-// 주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다.
-// 숫자들이 들어있는 배열 nums가 매개변수로 주어질 때,
-// nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return 하도록 solution 함수를 완성해주세요.
+// S사에서는 각 부서에 필요한 물품을 지원해 주기 위해 부서별로 물품을 구매하는데 필요한 금액을 조사했습니다.
+// 그러나, 전체 예산이 정해져 있기 때문에 모든 부서의 물품을 구매해 줄 수는 없습니다.
+// 그래서 최대한 많은 부서의 물품을 구매해 줄 수 있도록 하려고 합니다.
+//
+// 물품을 구매해 줄 때는 각 부서가 신청한 금액만큼을 모두 지원해 줘야 합니다.
+// 예를 들어 1,000원을 신청한 부서에는 정확히 1,000원을 지원해야 하며, 1,000원보다 적은 금액을 지원해 줄 수는 없습니다.
+//
+// 부서별로 신청한 금액이 들어있는 배열 d와 예산 budget이 매개변수로 주어질 때,
+// 최대 몇 개의 부서에 물품을 지원할 수 있는지 return 하도록 solution 함수를 완성해주세요.
 //
 // 제한사항
-// nums에 들어있는 숫자의 개수는 3개 이상 50개 이하입니다.
-// nums의 각 원소는 1 이상 1,000 이하의 자연수이며, 중복된 숫자가 들어있지 않습니다.
+// d는 부서별로 신청한 금액이 들어있는 배열이며, 길이(전체 부서의 개수)는 1 이상 100 이하입니다.
+// d의 각 원소는 부서별로 신청한 금액을 나타내며, 부서별 신청 금액은 1 이상 100,000 이하의 자연수입니다.
+// budget은 예산을 나타내며, 1 이상 10,000,000 이하의 자연수입니다.
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4};
-        solution(nums);
+        int[] d = {1,3,2,5,4};
+        int budget = 9;
+        solution(d, budget);
     }
 
-    public static int solution(int[] nums) {
+    public static int solution(int[] d, int budget) {
 
-        List<Integer> sumList = new ArrayList<>();
-        for(int i = 0; i < nums.length; i ++) {
-            for(int j = i+1; j < nums.length; j ++) {
-                for(int k = j+1; k < nums.length; k ++) {
-                    int sum = nums[i] + nums[j] + nums[k];
-                    sumList.add(sum);
-                }
-            }
-        }
+        Arrays.sort(d);
 
         int answer = 0;
-        for(Integer num : sumList) {
-            boolean isPrime = true;
-            for(int i = 2; i < num; i ++) {
-                if(num % i == 0) {
-                    isPrime = false;
-                    break;
-                }
+        for(int i : d) {
+            if(budget - i >= 0) {
+                budget -= i;
+                answer ++;
+            } else {
+                break;
             }
-            if(isPrime) answer ++;
         }
 
         return answer;
