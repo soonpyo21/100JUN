0fae24657d7e0ccd3d786449b5f50b6c335f81f5
soonpyo21 <tnsvy2004@gmail.com>
2024-06-24 23:15:09 +0900

    프로그래머스 - 체육복 (1)

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,55 @@
 // 문제
-// 수포자는 수학을 포기한 사람의 준말입니다.
-// 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다.
-// 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+// 점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다.
+// 다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려 합니다.
+// 학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다.
+// 예를 들어, 4번 학생은 3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다.
+// 체육복이 없으면 수업을 들을 수 없기 때문에 체육복을 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
 //
-// 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
-// 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
-// 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
-//
-// 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때,
-// 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+// 전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost,
+// 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때,
+// 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
 //
 // 제한사항
-// 시험은 최대 10,000 문제로 구성되어있습니다.
-// 문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
-// 가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
+// 전체 학생의 수는 2명 이상 30명 이하입니다.
+// 체육복을 도난당한 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+// 여벌의 체육복을 가져온 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+// 여벌 체육복이 있는 학생만 다른 학생에게 체육복을 빌려줄 수 있습니다.
+// 여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다.
+// 이때 이 학생은 체육복을 하나만 도난당했다고 가정하며,
+// 남은 체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다.
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] answers = {1,2,3,4,5};
-        solution(answers);
+        int n = 5;
+        int[] lost = {2,4};
+        int[] reserve = {1,3,5};
+        solution(n, lost, reserve);
     }
 
-    public static int[] solution(int[] answers) {
-        int[] supo1 = {1,2,3,4,5};
-        int[] supo2 = {2,1,2,3,2,4,2,5};
-        int[] supo3 = {3,3,1,1,2,2,4,4,5,5};
+    public static int solution(int n, int[] lost, int[] reserve) {
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
 
-        int point1 = 0;
-        int point2 = 0;
-        int point3 = 0;
+        for(int i = 0; i < lost.length; i ++) {
+            int lNum = lost[i];
 
-        int idx1 = 0;
-        int idx2 = 0;
-        int idx3 = 0;
+            for(int j = 0; j < reserve.length; j ++) {
+                int rNum = reserve[j];
 
-        for(int i = 0; i < answers.length; i ++) {
+                if(rNum - 1 <= lNum && rNum + 1 >= lNum) {
+                    reserve[j] = -1;
+                    break;
+                }
 
-            if(answers[i] == supo1[idx1]) point1 ++;
-            if(answers[i] == supo2[idx2]) point2 ++;
-            if(answers[i] == supo3[idx3]) point3 ++;
-
-            if(idx1 == supo1.length-1) idx1 = -1;
-            if(idx2 == supo2.length-1) idx2 = -1;
-            if(idx3 == supo3.length-1) idx3 = -1;
-
-            idx1 ++;
-            idx2 ++;
-            idx3 ++;
+                if(j == reserve.length - 1) n --;
+            }
         }
 
-        List<Integer> list = new ArrayList<>();
-        int max = Math.max(point1, point2);
-        max = Math.max(max, point3);
-
-        if(point1 == max) list.add(1);
-        if(point2 == max) list.add(2);
-        if(point3 == max) list.add(3);
-
-        int[] answer = new int[list.size()];
-
-        for(int i = 0; i < answer.length; i ++) {
-            answer[i] = list.get(i);
-        }
-
+        int answer = n;
         return answer;
     }
 }
