5aa0c7f02fd074a6f3de835f22318b761ec5dcd5
soonpyo21 <tnsvy2004@gmail.com>
2024-07-17 00:03:45 +0900

    프로그래머스 - JadenCase 문자열 만들기

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,41 @@
 // 문제
-// 길이가 같은 배열 A, B 두개가 있습니다. 각 배열은 자연수로 이루어져 있습니다.
-// 배열 A, B에서 각각 한 개의 숫자를 뽑아 두 수를 곱합니다. 이러한 과정을 배열의 길이만큼 반복하며,
-// 두 수를 곱한 값을 누적하여 더합니다. 이때 최종적으로 누적된 값이 최소가 되도록 만드는 것이 목표입니다.
-// (단, 각 배열에서 k번째 숫자를 뽑았다면 다음에 k번째 숫자는 다시 뽑을 수 없습니다.)
-//
-// 예를 들어 A = [1, 4, 2] , B = [5, 4, 4] 라면
-//
-// A에서 첫번째 숫자인 1, B에서 첫번째 숫자인 5를 뽑아 곱하여 더합니다. (누적된 값 : 0 + 5(1x5) = 5)
-// A에서 두번째 숫자인 4, B에서 세번째 숫자인 4를 뽑아 곱하여 더합니다. (누적된 값 : 5 + 16(4x4) = 21)
-// A에서 세번째 숫자인 2, B에서 두번째 숫자인 4를 뽑아 곱하여 더합니다. (누적된 값 : 21 + 8(2x4) = 29)
-// 즉, 이 경우가 최소가 되므로 29를 return 합니다.
-//
-// 배열 A, B가 주어질 때 최종적으로 누적된 최솟값을 return 하는 solution 함수를 완성해 주세요.
+// JadenCase란 모든 단어의 첫 문자가 대문자이고, 그 외의 알파벳은 소문자인 문자열입니다.
+// 단, 첫 문자가 알파벳이 아닐 때에는 이어지는 알파벳은 소문자로 쓰면 됩니다. (첫 번째 입출력 예 참고)
+// 문자열 s가 주어졌을 때, s를 JadenCase로 바꾼 문자열을 리턴하는 함수, solution을 완성해주세요.
 //
 // 제한사항
-// 배열 A, B의 크기 : 1,000 이하의 자연수
-// 배열 A, B의 원소의 크기 : 1,000 이하의 자연수
-
-import java.util.Arrays;
+// s는 길이 1 이상 200 이하인 문자열입니다.
+// s는 알파벳과 숫자, 공백문자(" ")로 이루어져 있습니다.
+// 숫자는 단어의 첫 문자로만 나옵니다.
+// 숫자로만 이루어진 단어는 없습니다.
+// 공백문자가 연속해서 나올 수 있습니다.
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] A = {1,4,2};
-        int[] B = {5,4,4};
+        String s = "3people unFollowed me";
 
-        solution(A, B);
+        solution(s);
     }
 
-    public static int solution(int[] A, int[] B) {
+    public static String solution(String s) {
 
-        Arrays.sort(A);
-        Arrays.sort(B);
+        String str = s.replace(" ", "-");
+        String answer = "";
 
-        int len = A.length;
-        int answer = 0;
-        for(int i = 0; i < len; i ++) {
-            answer += A[i] * B[len - (i + 1)];
+        for(int i = 0; i < str.length(); i ++) {
+            if(i == 0) {
+                answer += String.valueOf(str.charAt(i)).toUpperCase();
+            } else {
+                if(str.charAt(i) != '-' && str.charAt(i-1) == '-') {
+                    answer += String.valueOf(str.charAt(i)).toUpperCase();
+                } else {
+                    answer += String.valueOf(str.charAt(i)).toLowerCase();
+                }
+            }
         }
 
-        return answer;
+        return answer.replace("-"," ");
     }
-
 }
 
