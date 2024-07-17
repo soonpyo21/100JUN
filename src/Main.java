733a789b27c69cafaaa716e8567c10f6eab51af8
soonpyo21 <tnsvy2004@gmail.com>
2024-07-18 00:02:32 +0900

    프로그래머스 - 이진 변환 반복하기

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,44 @@
 // 문제
-// JadenCase란 모든 단어의 첫 문자가 대문자이고, 그 외의 알파벳은 소문자인 문자열입니다.
-// 단, 첫 문자가 알파벳이 아닐 때에는 이어지는 알파벳은 소문자로 쓰면 됩니다. (첫 번째 입출력 예 참고)
-// 문자열 s가 주어졌을 때, s를 JadenCase로 바꾼 문자열을 리턴하는 함수, solution을 완성해주세요.
+// 0과 1로 이루어진 어떤 문자열 x에 대한 이진 변환을 다음과 같이 정의합니다.
+//
+// x의 모든 0을 제거합니다.
+// x의 길이를 c라고 하면, x를 "c를 2진법으로 표현한 문자열"로 바꿉니다.
+// 예를 들어, x = "0111010"이라면, x에 이진 변환을 가하면 x = "0111010" -> "1111" -> "100" 이 됩니다.
+//
+// 0과 1로 이루어진 문자열 s가 매개변수로 주어집니다.
+// s가 "1"이 될 때까지 계속해서 s에 이진 변환을 가했을 때,
+// 이진 변환의 횟수와 변환 과정에서 제거된 모든 0의 개수를 각각 배열에 담아 return 하도록 solution 함수를 완성해주세요.
 //
 // 제한사항
-// s는 길이 1 이상 200 이하인 문자열입니다.
-// s는 알파벳과 숫자, 공백문자(" ")로 이루어져 있습니다.
-// 숫자는 단어의 첫 문자로만 나옵니다.
-// 숫자로만 이루어진 단어는 없습니다.
-// 공백문자가 연속해서 나올 수 있습니다.
+// s의 길이는 1 이상 150,000 이하입니다.
+// s에는 '1'이 최소 하나 이상 포함되어 있습니다.
 
 public class Main {
 
     public static void main(String[] args) {
-        String s = "3people unFollowed me";
+        String s = "110010101001";
 
         solution(s);
     }
 
-    public static String solution(String s) {
+    public static int[] solution(String s) {
 
-        String str = s.replace(" ", "-");
-        String answer = "";
+        int loop = 0;
+        int del = 0;
 
-        for(int i = 0; i < str.length(); i ++) {
-            if(i == 0) {
-                answer += String.valueOf(str.charAt(i)).toUpperCase();
-            } else {
-                if(str.charAt(i) != '-' && str.charAt(i-1) == '-') {
-                    answer += String.valueOf(str.charAt(i)).toUpperCase();
-                } else {
-                    answer += String.valueOf(str.charAt(i)).toLowerCase();
-                }
+        while(!"1".equals(s)) {
+
+            for(int i = 0; i < s.length(); i ++) {
+                if(s.charAt(i) == '0') del ++;
             }
+
+            int len = s.replace("0","").length();
+            s = Integer.toBinaryString(len);
+            loop ++;
         }
 
-        return answer.replace("-"," ");
+        int[] answer = {loop, del};
+        return answer;
     }
 }
 
