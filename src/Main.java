a28c744024bd04ff47dd9a4359b09cda8a2339ea
soonpyo21 <tnsvy2004@gmail.com>
2024-07-19 23:48:54 +0900

    프로그래머스 - 숫자의 표현

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,46 @@
 // 문제
-// 0과 1로 이루어진 어떤 문자열 x에 대한 이진 변환을 다음과 같이 정의합니다.
+// Finn은 요즘 수학공부에 빠져 있습니다.
+// 수학 공부를 하던 Finn은 자연수 n을 연속한 자연수들로 표현 하는 방법이 여러개라는 사실을 알게 되었습니다.
+// 예를들어 15는 다음과 같이 4가지로 표현 할 수 있습니다.
 //
-// x의 모든 0을 제거합니다.
-// x의 길이를 c라고 하면, x를 "c를 2진법으로 표현한 문자열"로 바꿉니다.
-// 예를 들어, x = "0111010"이라면, x에 이진 변환을 가하면 x = "0111010" -> "1111" -> "100" 이 됩니다.
-//
-// 0과 1로 이루어진 문자열 s가 매개변수로 주어집니다.
-// s가 "1"이 될 때까지 계속해서 s에 이진 변환을 가했을 때,
-// 이진 변환의 횟수와 변환 과정에서 제거된 모든 0의 개수를 각각 배열에 담아 return 하도록 solution 함수를 완성해주세요.
+// 1 + 2 + 3 + 4 + 5 = 15
+// 4 + 5 + 6 = 15
+// 7 + 8 = 15
+// 15 = 15
+// 자연수 n이 매개변수로 주어질 때, 연속된 자연수들로 n을 표현하는 방법의 수를 return하는 solution를 완성해주세요.
 //
 // 제한사항
-// s의 길이는 1 이상 150,000 이하입니다.
-// s에는 '1'이 최소 하나 이상 포함되어 있습니다.
+// n은 10,000 이하의 자연수 입니다.
 
 public class Main {
 
     public static void main(String[] args) {
-        String s = "110010101001";
+        int n = 15;
 
-        solution(s);
+        solution(n);
     }
 
-    public static int[] solution(String s) {
+    public static int solution(int n) {
 
-        int loop = 0;
-        int del = 0;
+        int answer = 0;
 
-        while(!"1".equals(s)) {
-
-            for(int i = 0; i < s.length(); i ++) {
-                if(s.charAt(i) == '0') del ++;
-            }
-
-            int len = s.replace("0","").length();
-            s = Integer.toBinaryString(len);
-            loop ++;
+        for(int i = 1; i <= n; i ++) {
+            answer += findN(n, i);
         }
 
-        int[] answer = {loop, del};
         return answer;
+    }
+
+    private static int findN(int n, int start) {
+
+        int num = 0;
+        for(int i = start; i <= n; i ++) {
+            num += i;
+            if(num > n) return 0;
+            if(num == n) return 1;
+        }
+
+        return 0;
     }
 }
 
