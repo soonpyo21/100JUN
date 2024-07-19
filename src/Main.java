b8b187ef41a61ac66b922a0c8f1414ae5d90b90d
soonpyo21 <tnsvy2004@gmail.com>
2024-07-20 00:17:13 +0900

    프로그래머스 - 다음 큰 숫자

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,51 @@
 // 문제
-// Finn은 요즘 수학공부에 빠져 있습니다.
-// 수학 공부를 하던 Finn은 자연수 n을 연속한 자연수들로 표현 하는 방법이 여러개라는 사실을 알게 되었습니다.
-// 예를들어 15는 다음과 같이 4가지로 표현 할 수 있습니다.
+// 자연수 n이 주어졌을 때, n의 다음 큰 숫자는 다음과 같이 정의 합니다.
 //
-// 1 + 2 + 3 + 4 + 5 = 15
-// 4 + 5 + 6 = 15
-// 7 + 8 = 15
-// 15 = 15
-// 자연수 n이 매개변수로 주어질 때, 연속된 자연수들로 n을 표현하는 방법의 수를 return하는 solution를 완성해주세요.
+// 조건 1. n의 다음 큰 숫자는 n보다 큰 자연수 입니다.
+// 조건 2. n의 다음 큰 숫자와 n은 2진수로 변환했을 때 1의 갯수가 같습니다.
+// 조건 3. n의 다음 큰 숫자는 조건 1, 2를 만족하는 수 중 가장 작은 수 입니다.
+// 예를 들어서 78(1001110)의 다음 큰 숫자는 83(1010011)입니다.
+//
+// 자연수 n이 매개변수로 주어질 때, n의 다음 큰 숫자를 return 하는 solution 함수를 완성해주세요.
 //
 // 제한사항
-// n은 10,000 이하의 자연수 입니다.
+// n은 1,000,000 이하의 자연수 입니다.
 
 public class Main {
 
     public static void main(String[] args) {
-        int n = 15;
+        int n = 78;
 
         solution(n);
     }
 
     public static int solution(int n) {
 
-        int answer = 0;
-
-        for(int i = 1; i <= n; i ++) {
-            answer += findN(n, i);
+        String binaryN = Integer.toBinaryString(n);
+        int nCnt = 0;
+        for(int i = 0; i < binaryN.length(); i ++) {
+            if(binaryN.charAt(i) == '1') nCnt ++;
         }
 
+        int num = n + 1;
+        while(true) {
+
+            int numCnt = 0;
+            String binaryNum = Integer.toBinaryString(num);
+            for(int i = 0; i < binaryNum.length(); i ++) {
+                if(binaryNum.charAt(i) == '1') numCnt ++;
+            }
+
+            if(nCnt == numCnt) {
+                break;
+            } else {
+                num ++;
+            }
+        }
+
+        int answer = num;
         return answer;
     }
 
-    private static int findN(int n, int start) {
-
-        int num = 0;
-        for(int i = start; i <= n; i ++) {
-            num += i;
-            if(num > n) return 0;
-            if(num == n) return 1;
-        }
-
-        return 0;
-    }
 }
 
