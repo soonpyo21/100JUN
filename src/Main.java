485b58c90a8f965d007532b7440c8a1bdf9f8c58
soonpyo21 <tnsvy2004@gmail.com>
2024-09-01 18:17:40 +0900

    프로그래머스 - 멀리 뛰기 - 피보나치 수열 대표 유형

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,41 @@
 // 문제
-// 무인도에 갇힌 사람들을 구명보트를 이용하여 구출하려고 합니다. 구명보트는 작아서 한 번에 최대 2명씩 밖에 탈 수 없고, 무게 제한도 있습니다.
-// 예를 들어, 사람들의 몸무게가 [70kg, 50kg, 80kg, 50kg]이고 구명보트의 무게 제한이 100kg이라면
-// 2번째 사람과 4번째 사람은 같이 탈 수 있지만 1번째 사람과 3번째 사람의 무게의 합은 150kg이므로
-// 구명보트의 무게 제한을 초과하여 같이 탈 수 없습니다.
+// 효진이는 멀리 뛰기를 연습하고 있습니다. 효진이는 한번에 1칸, 또는 2칸을 뛸 수 있습니다.
+// 칸이 총 4개 있을 때, 효진이는
+// (1칸, 1칸, 1칸, 1칸)
+// (1칸, 2칸, 1칸)
+// (1칸, 1칸, 2칸)
+// (2칸, 1칸, 1칸)
+// (2칸, 2칸)
+// 의 5가지 방법으로 맨 끝 칸에 도달할 수 있습니다.
+// 멀리뛰기에 사용될 칸의 수 n이 주어질 때, 효진이가 끝에 도달하는 방법이 몇 가지인지 알아내,
+// 여기에 1234567를 나눈 나머지를 리턴하는 함수, solution을 완성하세요.
+// 예를 들어 4가 입력된다면, 5를 return하면 됩니다.
 //
-// 구명보트를 최대한 적게 사용하여 모든 사람을 구출하려고 합니다.
-//
-// 사람들의 몸무게를 담은 배열 people과 구명보트의 무게 제한 limit가 매개변수로 주어질 때,
-// 모든 사람을 구출하기 위해 필요한 구명보트 개수의 최솟값을 return 하도록 solution 함수를 작성해주세요.
-//
-// 제한사항
-// 무인도에 갇힌 사람은 1명 이상 50,000명 이하입니다.
-// 각 사람의 몸무게는 40kg 이상 240kg 이하입니다.
-// 구명보트의 무게 제한은 40kg 이상 240kg 이하입니다.
-// 구명보트의 무게 제한은 항상 사람들의 몸무게 중 최댓값보다 크게 주어지므로 사람들을 구출할 수 없는 경우는 없습니다.
-
-import java.util.Arrays;
+// 제한 사항
+// n은 1 이상, 2000 이하인 정수입니다.
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] people = {70, 50, 80, 50};
-        int limit = 100;
+        int n = 4;
 
-        solution(people, limit);
+        solution(n);
     }
 
-    public static int solution(int[] people, int limit) {
+    public static long solution(int n) {
 
-        int index = 0;
-        Arrays.sort(people);
+        if(n == 1) return 1;
+        if(n == 2) return 2;
 
-        int answer = 0;
-        for(int i = people.length - 1; i >= index; i --) {
-            if(people[i] + people[index] <= limit) {
-                answer ++;
-                index ++;
-            } else {
-                answer ++;
-            }
+        int[] dp = new int[n];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n; i ++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
         }
 
-        return answer;
+        return dp[n];
     }
 
 }
