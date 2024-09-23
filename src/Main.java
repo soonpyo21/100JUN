16dd2ed423818cd187bfaac70d6cc7e132dae50a
soonpyo21 <tnsvy2004@gmail.com>
2024-09-23 22:39:50 +0900

    프로그래머스 - 영어 끝말잇기

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,85 @@
-// 문제
-// 두 수의 최소공배수(Least Common Multiple)란 입력된 두 수의 배수 중 공통이 되는 가장 작은 숫자를 의미합니다.
-// 예를 들어 2와 7의 최소공배수는 14가 됩니다.
-// 정의를 확장해서, n개의 수의 최소공배수는 n 개의 수들의 배수 중 공통이 되는 가장 작은 숫자가 됩니다.
-// n개의 숫자를 담은 배열 arr이 입력되었을 때 이 수들의 최소공배수를 반환하는 함수, solution을 완성해 주세요.
+// 240923 문제
+// 1부터 n까지 번호가 붙어있는 n명의 사람이 영어 끝말잇기를 하고 있습니다. 영어 끝말잇기는 다음과 같은 규칙으로 진행됩니다.
+//
+// 1번부터 번호 순서대로 한 사람씩 차례대로 단어를 말합니다.
+// 마지막 사람이 단어를 말한 다음에는 다시 1번부터 시작합니다.
+// 앞사람이 말한 단어의 마지막 문자로 시작하는 단어를 말해야 합니다.
+// 이전에 등장했던 단어는 사용할 수 없습니다.
+// 한 글자인 단어는 인정되지 않습니다.
+// 다음은 3명이 끝말잇기를 하는 상황을 나타냅니다.
+//
+// tank → kick → know → wheel → land → dream → mother → robot → tank
+//
+// 위 끝말잇기는 다음과 같이 진행됩니다.
+//
+// 1번 사람이 자신의 첫 번째 차례에 tank를 말합니다.
+// 2번 사람이 자신의 첫 번째 차례에 kick을 말합니다.
+// 3번 사람이 자신의 첫 번째 차례에 know를 말합니다.
+// 1번 사람이 자신의 두 번째 차례에 wheel을 말합니다.
+// (계속 진행)
+// 끝말잇기를 계속 진행해 나가다 보면, 3번 사람이 자신의 세 번째 차례에 말한 tank 라는 단어는 이전에 등장했던 단어이므로 탈락하게 됩니다.
+//
+// 사람의 수 n과 사람들이 순서대로 말한 단어 words 가 매개변수로 주어질 때,
+// 가장 먼저 탈락하는 사람의 번호와 그 사람이 자신의 몇 번째 차례에 탈락하는지를 구해서 return 하도록 solution 함수를 완성해주세요.
 //
 // 제한 사항
-// arr은 길이 1이상, 15이하인 배열입니다.
-// arr의 원소는 100 이하인 자연수입니다.
+// 끝말잇기에 참여하는 사람의 수 n은 2 이상 10 이하의 자연수입니다.
+// words는 끝말잇기에 사용한 단어들이 순서대로 들어있는 배열이며, 길이는 n 이상 100 이하입니다.
+// 단어의 길이는 2 이상 50 이하입니다.
+// 모든 단어는 알파벳 소문자로만 이루어져 있습니다.
+// 끝말잇기에 사용되는 단어의 뜻(의미)은 신경 쓰지 않으셔도 됩니다.
+// 정답은 [ 번호, 차례 ] 형태로 return 해주세요.
+// 만약 주어진 단어들로 탈락자가 생기지 않는다면, [0, 0]을 return 해주세요.
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {2,6,8,14};
+        int n = 3;
+        String[] words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
 
-        solution(arr);
+        int[] answer = solution(n, words);
     }
 
-    public static int solution(int[] arr) {
+    public static int[] solution(int n, String[] words) {
 
-        int answer = arr[0];
+        List<String> useList = new ArrayList<>();   // 사용된 단어 리스트
+        useList.add(words[0]);
 
-         for(int i = 1; i < arr.length; i ++) {
-             answer = lcm(answer, arr[i]);
-         }
+        for(int i = 1; i < words.length; i ++) {
 
-        System.out.println(answer);
-        return answer;
-    }
+            String curWord = words[i];  // 현재 단어
 
-    private static int lcm(int a, int b) {
-        return a * b / gcd(a,b);
-    }
+            // 1. 한 글자인 단어인지 체크
+            if(curWord.length() == 1) {
+                return new int[]{(i % n) + 1, (i / n) + 1};
+            }
 
-    private static int gcd(int a, int b) {
-        if(a % b == 0) {
-            return b;
-        } else {
-            return gcd(b, a % b);
+            // 2.이전 단어의 끝말인지 체크
+            String lastWord = words[i-1];   // 이전 단어
+
+            String lastSpel = String.valueOf(lastWord.charAt(lastWord.length()-1)); // 이전 단어의 마지막 스펠링
+            String curSpel = String.valueOf(curWord.charAt(0));                     // 현재 단어의 첫번째 스펠링
+
+            if(!lastSpel.equals(curSpel)) {
+                return new int[]{(i % n) + 1, (i / n) + 1};
+            }
+
+            // 3. 사용된 단어인지 체크
+            for(int j = 0; j < useList.size(); j ++) {
+                if(curWord.equals(useList.get(j))) {
+                    return new int[]{(i % n) + 1, (i / n) + 1};
+                }
+            }
+
+            // 사용되지 않았다면 사용 단어 리스트에 add
+            useList.add(curWord);
         }
+
+        // 탈락자가 발생하지 않는 경우 0,0 return
+        return new int[]{0, 0};
     }
 
 }
-
