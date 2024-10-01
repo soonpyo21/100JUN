e7f9c34ebd90509e9a52f814a5aaa3acae1c6df9
soonpyo21 <tnsvy2004@gmail.com>
2024-10-02 01:34:46 +0900

    프로그래머스 - 행렬의 곱셈

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,31 @@
 // 문제
 //
-// 정수 n, left, right가 주어집니다. 다음 과정을 거쳐서 1차원 배열을 만들고자 합니다.
+// 2차원 행렬 arr1과 arr2를 입력받아, arr1에 arr2를 곱한 결과를 반환하는 함수, solution을 완성해주세요.
 //
-// n행 n열 크기의 비어있는 2차원 배열을 만듭니다.
-// i = 1, 2, 3, ..., n에 대해서, 다음 과정을 반복합니다.
-// 1행 1열부터 i행 i열까지의 영역 내의 모든 빈 칸을 숫자 i로 채웁니다.
-// 1행, 2행, ..., n행을 잘라내어 모두 이어붙인 새로운 1차원 배열을 만듭니다.
-// 새로운 1차원 배열을 arr이라 할 때, arr[left], arr[left+1], ..., arr[right]만 남기고 나머지는 지웁니다.
-// 정수 n, left, right가 매개변수로 주어집니다. 주어진 과정대로 만들어진 1차원 배열을 return 하도록 solution 함수를 완성해주세요.
-//
-// 제한 사항
-// 1 ≤ n ≤ 107
-// 0 ≤ left ≤ right < n2
-// right - left < 105
+// 제한 조건
+// 행렬 arr1, arr2의 행과 열의 길이는 2 이상 100 이하입니다.
+// 행렬 arr1, arr2의 원소는 -10 이상 20 이하인 자연수입니다.
+// 곱할 수 있는 배열만 주어집니다.
 
 public class Main {
 
     public static void main(String[] args) {
-        int n = 3;
-        long left = 2;
-        long right = 5;
+        int[][] arr1 = {{1,4}, {3,2}, {4,1}};
+        int[][] arr2 = {{3,3}, {3,3}};
 
-        solution(n, left, right);
+        solution(arr1, arr2);
     }
 
-    public static int[] solution(int n, long left, long right) {
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
 
-        int[] answer = new int[(int) (right - left + 1)];
-        int idx = 0;
+        int[][] answer = new int[arr1.length][arr2[0].length];
 
-        for(long i = left; i <= right; i ++) {
-            long row = i / n + 1;
-            long col = i % n + 1;
-
-            answer[idx] = (int) Math.max(row, col);
-            idx ++;
+        for(int i = 0; i < arr1.length; i ++) {
+            for(int j = 0; j < arr2[0].length; j ++) {
+                for(int k = 0; k < arr1[0].length; k ++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
         }
 
         return answer;
