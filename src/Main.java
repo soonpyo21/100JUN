8363b5de501fdad636873068d7a5d07aeeee69b5
soonpyo21 <tnsvy2004@gmail.com>
2024-10-13 16:41:52 +0900

    프로그래머스 - 튜플

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,68 @@
 // 문제
-// 지도개발팀에서 근무하는 제이지는 지도에서 도시 이름을 검색하면
-// 해당 도시와 관련된 맛집 게시물들을 데이터베이스에서 읽어 보여주는 서비스를 개발하고 있다.
-// 이 프로그램의 테스팅 업무를 담당하고 있는 어피치는 서비스를 오픈하기 전 각 로직에 대한 성능 측정을 수행하였는데,
-// 제이지가 작성한 부분 중 데이터베이스에서 게시물을 가져오는 부분의 실행시간이 너무 오래 걸린다는 것을 알게 되었다.
-// 어피치는 제이지에게 해당 로직을 개선하라고 닦달하기 시작하였고,
-// 제이지는 DB 캐시를 적용하여 성능 개선을 시도하고 있지만 캐시 크기를 얼마로 해야 효율적인지 몰라 난감한 상황이다.
+// 셀수있는 수량의 순서있는 열거 또는 어떤 순서를 따르는 요소들의 모음을 튜플(tuple)이라고 합니다.
+// n개의 요소를 가진 튜플을 n-튜플(n-tuple)이라고 하며, 다음과 같이 표현할 수 있습니다.
 //
-// 어피치에게 시달리는 제이지를 도와, DB 캐시를 적용할 때 캐시 크기에 따른 실행시간 측정 프로그램을 작성하시오.
+// (a1, a2, a3, ..., an)
+// 튜플은 다음과 같은 성질을 가지고 있습니다.
 //
-// 입력 형식
-// 캐시 크기(cacheSize)와 도시이름 배열(cities)을 입력받는다.
-// cacheSize는 정수이며, 범위는 0 ≦ cacheSize ≦ 30 이다.
-// cities는 도시 이름으로 이뤄진 문자열 배열로, 최대 도시 수는 100,000개이다.
-// 각 도시 이름은 공백, 숫자, 특수문자 등이 없는 영문자로 구성되며, 대소문자 구분을 하지 않는다. 도시 이름은 최대 20자로 이루어져 있다.
+// 중복된 원소가 있을 수 있습니다. ex : (2, 3, 1, 2)
+// 원소에 정해진 순서가 있으며, 원소의 순서가 다르면 서로 다른 튜플입니다. ex : (1, 2, 3) ≠ (1, 3, 2)
+// 튜플의 원소 개수는 유한합니다.
+// 원소의 개수가 n개이고, 중복되는 원소가 없는 튜플 (a1, a2, a3, ..., an)이 주어질 때(단, a1, a2, ..., an은 자연수),
+// 이는 다음과 같이 집합 기호 '{', '}'를 이용해 표현할 수 있습니다.
 //
-// 출력 형식
-// 입력된 도시이름 배열을 순서대로 처리할 때, "총 실행시간"을 출력한다.
+// {{a1}, {a1, a2}, {a1, a2, a3}, {a1, a2, a3, a4}, ... {a1, a2, a3, a4, ..., an}}
+// 예를 들어 튜플이 (2, 1, 3, 4)인 경우 이는
+// {{2}, {2, 1}, {2, 1, 3}, {2, 1, 3, 4}}
+// 와 같이 표현할 수 있습니다. 이때, 집합은 원소의 순서가 바뀌어도 상관없으므로
+//
+// {{2}, {2, 1}, {2, 1, 3}, {2, 1, 3, 4}}
+// {{2, 1, 3, 4}, {2}, {2, 1, 3}, {2, 1}}
+// {{1, 2, 3}, {2, 1}, {1, 2, 4, 3}, {2}}
+// 는 모두 같은 튜플 (2, 1, 3, 4)를 나타냅니다.
+//
+// 특정 튜플을 표현하는 집합이 담긴 문자열 s가 매개변수로 주어질 때,
+// s가 표현하는 튜플을 배열에 담아 return 하도록 solution 함수를 완성해주세요.
 //
 // 제한 조건
-// 캐시 교체 알고리즘은 LRU(Least Recently Used)를 사용한다.
-// cache hit일 경우 실행시간은 1이다.
-// cache miss일 경우 실행시간은 5이다.
+// s의 길이는 5 이상 1,000,000 이하입니다.
+// s는 숫자와 '{', '}', ',' 로만 이루어져 있습니다.
+// 숫자가 0으로 시작하는 경우는 없습니다.
+// s는 항상 중복되는 원소가 없는 튜플을 올바르게 표현하고 있습니다.
+// s가 표현하는 튜플의 원소는 1 이상 100,000 이하인 자연수입니다.
+// return 하는 배열의 길이가 1 이상 500 이하인 경우만 입력으로 주어집니다.
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+        String s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
 
-        int cacheSize = 3;
-        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
-
-        solution(cacheSize, cities);
+        solution(s);
     }
 
-    private static int solution(int cacheSize, String[] cities) {
+    private static int[] solution(String s) {
+        String str = s.substring(2, s.length()-2);
+        String[] sArr = str.split("\\},\\{");
 
-        List<String> list = new ArrayList<>(); // 가상의 메모리 공간
-        int answer = 0;                        // 실행 시간
+        Arrays.sort(sArr, (a,b) -> a.length() - b.length());
 
-        // 저장 공간이 0일 경우 모든 케이스가 cache miss
-        if(cacheSize == 0) return cities.length * 5;
+        List<Integer> list = new ArrayList<>();
+        list.add(Integer.parseInt(sArr[0]));
 
-        for(int i = 0; i < cities.length; i ++) {
-            String city = cities[i].toLowerCase();  // 대소문자 구분이 없으므로 도시명 통일
-
-            if(list.contains(city)) {   // cache hit
-                // 해당 요소를 삭제 후 다시 추가
-                for(int j = 0; j < list.size(); j ++) {
-                    if(list.get(j).equals(city)) {
-                        list.remove(j);
-                        list.add(city);
-                        answer += 1;
-                        break;
-                    }
+        for(int i = 1; i < sArr.length; i ++) {
+            String[] arr = sArr[i].split(",");
+            for(int j = 0; j < arr.length; j ++) {
+                if(!list.contains(Integer.parseInt(arr[j]))) {
+                    list.add(Integer.parseInt(arr[j]));
                 }
-
-            } else {    // cache miss
-                // 저장 공간이 full일 경우 첫번째 요소 삭제 후 추가
-                if(list.size() >= cacheSize) list.remove(0);
-                list.add(city);
-                answer += 5;
             }
         }
+
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
 
         return answer;
     }
