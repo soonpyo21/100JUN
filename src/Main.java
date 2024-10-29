14a235f3a8ff2844cb98bc8b5ea86fe58f3570f1
soonpyo21 <tnsvy2004@gmail.com>
2024-10-29 23:10:29 +0900

    백준 - 단지번호붙이기

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,98 @@
-// 문제 (백준-바이러스)
-// 신종 바이러스인 웜 바이러스는 네트워크를 통해 전파된다. 한 컴퓨터가 웜 바이러스에 걸리면 그 컴퓨터와 네트워크 상에서 연결되어 있는 모든 컴퓨터는 웜 바이러스에 걸리게 된다.
-// 예를 들어 7대의 컴퓨터가 <그림 1>과 같이 네트워크 상에서 연결되어 있다고 하자.
-// 1번 컴퓨터가 웜 바이러스에 걸리면 웜 바이러스는 2번과 5번 컴퓨터를 거쳐 3번과 6번 컴퓨터까지 전파되어 2, 3, 5, 6 네 대의 컴퓨터는 웜 바이러스에 걸리게 된다.
-// 하지만 4번과 7번 컴퓨터는 1번 컴퓨터와 네트워크상에서 연결되어 있지 않기 때문에 영향을 받지 않는다.
-// 어느 날 1번 컴퓨터가 웜 바이러스에 걸렸다. 컴퓨터의 수와 네트워크 상에서 서로 연결되어 있는 정보가 주어질 때,
-// 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를 출력하는 프로그램을 작성하시오.
+// 문제 (단지번호붙이기)
+// <그림 1>과 같이 정사각형 모양의 지도가 있다.
+// 1은 집이 있는 곳을, 0은 집이 없는 곳을 나타낸다.
+// 철수는 이 지도를 가지고 연결된 집의 모임인 단지를 정의하고, 단지에 번호를 붙이려 한다.
+// 여기서 연결되었다는 것은 어떤 집이 좌우, 혹은 아래위로 다른 집이 있는 경우를 말한다. 대각선상에 집이 있는 경우는 연결된 것이 아니다.
+// <그림 2>는 <그림 1>을 단지별로 번호를 붙인 것이다.
+// 지도를 입력하여 단지수를 출력하고, 각 단지에 속하는 집의 수를 오름차순으로 정렬하여 출력하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄에는 컴퓨터의 수가 주어진다. 컴퓨터의 수는 100 이하인 양의 정수이고 각 컴퓨터에는 1번 부터 차례대로 번호가 매겨진다.
-// 둘째 줄에는 네트워크 상에서 직접 연결되어 있는 컴퓨터 쌍의 수가 주어진다.
-// 이어서 그 수만큼 한 줄에 한 쌍씩 네트워크 상에서 직접 연결되어 있는 컴퓨터의 번호 쌍이 주어진다.
+// 첫 번째 줄에는 지도의 크기 N(정사각형이므로 가로와 세로의 크기는 같으며 5≤N≤25)이 입력되고, 그 다음 N줄에는 각각 N개의 자료(0혹은 1)가 입력된다.
 //
 // 출력
-// 1번 컴퓨터가 웜 바이러스에 걸렸을 때, 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를 첫째 줄에 출력한다
+// 첫 번째 줄에는 총 단지수를 출력하시오. 그리고 각 단지내 집의 수를 오름차순으로 정렬하여 한 줄에 하나씩 출력하시오.
 
-import java.util.*;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
 
-    static int answer = 0;  // 1번 컴퓨터에 의해 감염된 컴퓨터의 수
+    private static int N;
+    private static List<ArrayList<Integer>> list = new ArrayList<>();
+    private static int[] dx = {-1,1,0,0};
+    private static int[] dy = {0,0,-1,1};
 
     public static void main(String[] args) {
         solution();
     }
 
     private static void solution() {
+
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();   // 컴퓨터의 총 갯수
-        int M = sc.nextInt();   // 연결된 컴퓨터의 간선 갯수
+        N = sc.nextInt();
         sc.nextLine();
 
-        List<ArrayList<Integer>> list = new ArrayList<>();  // 각 컴퓨터의 연결 정보를 담을 list
-        // 컴퓨터의 갯수만큼 list 초기화
-        for(int i = 0; i <= N; i ++) {
-            list.add(i, new ArrayList<>());
-        }
+        // 지도를 담을 list 세팅
+        for(int i = 0; i < N; i ++) {
+            list.add(new ArrayList<>());
+            String[] arr = sc.nextLine().split("");
 
-        // 입력된 컴퓨터의 연결 정보 list에 저장
-        for(int i = 1; i <= M; i ++) {
-            String[] input = sc.nextLine().split(" ");
-            int s = Integer.parseInt(input[0]);
-            int e = Integer.parseInt(input[1]);
-
-            list.get(s).add(e);
-            list.get(e).add(s);
-        }
-
-        /* list 출력
-        for(int i = 0; i < list.size(); i ++) {
-            for(int j = 0; j < list.get(i).size(); j ++) {
-                System.out.print(list.get(i).get(j));
+            for(int j = 0; j < N; j ++) {
+                list.get(i).add(Integer.parseInt(arr[j]));
             }
-            System.out.println();
         }
-        */
 
-        boolean[] visit = new boolean[list.size()]; // 방문 여부를 표시할 배열
-        dfs(list, visit, 1);
-        System.out.println(answer);
+        int[][] visit = new int[N][N];            // 방문 여부를 표시할 배열
+        List<Integer> answer = new ArrayList<>(); // 결과를 담을 list
+
+        for(int i = 0; i < N; i ++) {
+            for(int j = 0; j < N; j ++) {
+                if(list.get(i).get(j) == 1 && visit[i][j] == 0) {
+                    answer.add(bfs(i, j, visit));
+                }
+            }
+        }
+
+        Collections.sort(answer);
+        answer.add(0, answer.size());
+
+        for(int i : answer) System.out.println(i);
     }
 
-    private static void dfs(List<ArrayList<Integer>> list, boolean[] visit, int V) {
-        visit[V] = true;
+    private static int bfs(int i, int j, int[][] visit) {
+        Queue<Point> q = new LinkedList<>();    // bfs 탐색을 위한 큐 생성
+        q.offer(new Point(i, j));               // 초기값 세팅
+        visit[i][j] = 1;                        // 방문 체크
+        int cnt = 1;                            // 탐색 중인 단지 안의 집 개수
 
-        for(int i = 0; i < list.get(V).size(); i ++) {
-            if(visit[list.get(V).get(i)] != true) { // 방문한 적 없는 노드라면 컴퓨터 카운팅 후 재탐색
-                answer ++;
-                dfs(list, visit, list.get(V).get(i));
+        while(!q.isEmpty()) {
+            Point p = q.poll();
+            int x = p.x;
+            int y = p.y;
+
+            // 상, 하, 좌, 우 네방향 이동 체크
+            for(int k = 0; k < 4; k ++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;    // 지도를 벗어났다면 무시
+                if(list.get(nx).get(ny) == 0) continue;                 // 집이 없는 곳이라면 무시
+
+                if(visit[nx][ny] == 0) {        // 방문하지 않은 집인 경우
+                    q.offer(new Point(nx,ny));
+                    visit[nx][ny] = 1;
+                    cnt ++;
+                }
             }
         }
+
+        return cnt;
     }
 
 }
