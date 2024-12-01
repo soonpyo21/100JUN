0b3903cc1b76bf5ab1065b8d32ef7adad191d853
soonpyo21 <tnsvy2004@gmail.com>
2024-12-01 17:31:10 +0900

    백준 - 안전영역

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,57 @@
-// 문제 (스타트링크)
-// 강호는 코딩 교육을 하는 스타트업 스타트링크에 지원했다. 오늘은 강호의 면접날이다. 하지만, 늦잠을 잔 강호는 스타트링크가 있는 건물에 늦게 도착하고 말았다.
-// 스타트링크는 총 F층으로 이루어진 고층 건물에 사무실이 있고, 스타트링크가 있는 곳의 위치는 G층이다.
-// 강호가 지금 있는 곳은 S층이고, 이제 엘리베이터를 타고 G층으로 이동하려고 한다.
-// 보통 엘리베이터에는 어떤 층으로 이동할 수 있는 버튼이 있지만, 강호가 탄 엘리베이터는 버튼이 2개밖에 없다.
-// U버튼은 위로 U층을 가는 버튼, D버튼은 아래로 D층을 가는 버튼이다. (만약, U층 위, 또는 D층 아래에 해당하는 층이 없을 때는, 엘리베이터는 움직이지 않는다)
-// 강호가 G층에 도착하려면, 버튼을 적어도 몇 번 눌러야 하는지 구하는 프로그램을 작성하시오.
-// 만약, 엘리베이터를 이용해서 G층에 갈 수 없다면, "use the stairs"를 출력한다.
+// 문제 (안전 영역)
+// 재난방재청에서는 많은 비가 내리는 장마철에 대비해서 다음과 같은 일을 계획하고 있다. 먼저 어떤 지역의 높이 정보를 파악한다.
+// 그 다음에 그 지역에 많은 비가 내렸을 때 물에 잠기지 않는 안전한 영역이 최대로 몇 개가 만들어 지는 지를 조사하려고 한다.
+// 이때, 문제를 간단하게 하기 위하여, 장마철에 내리는 비의 양에 따라 일정한 높이 이하의 모든 지점은 물에 잠긴다고 가정한다.
+//
+// 어떤 지역의 높이 정보는 행과 열의 크기가 각각 N인 2차원 배열 형태로 주어지며 배열의 각 원소는 해당 지점의 높이를 표시하는 자연수이다.
+// 예를 들어, 다음은 N=5인 지역의 높이 정보이다.
+//
+// 6	8	2	6	2
+// 3	2	3	4	6
+// 6	7	3	3	2
+// 7	2	5	3	6
+// 8	9	5	2	7
+// 이제 위와 같은 지역에 많은 비가 내려서 높이가 4 이하인 모든 지점이 물에 잠겼다고 하자. 이 경우에 물에 잠기는 지점을 회색으로 표시하면 다음과 같다.
+//
+// 6	8	2	6	2
+// 3	2	3	4	6
+// 6	7	3	3	2
+// 7	2	5	3	6
+// 8	9	5	2	7
+// 물에 잠기지 않는 안전한 영역이라 함은 물에 잠기지 않는 지점들이 위, 아래, 오른쪽 혹은 왼쪽으로 인접해 있으며 그 크기가 최대인 영역을 말한다.
+// 위의 경우에서 물에 잠기지 않는 안전한 영역은 5개가 된다(꼭짓점으로만 붙어 있는 두 지점은 인접하지 않는다고 취급한다).
+//
+// 또한 위와 같은 지역에서 높이가 6이하인 지점을 모두 잠기게 만드는 많은 비가 내리면 물에 잠기지 않는 안전한 영역은 아래 그림에서와 같이 네 개가 됨을 확인할 수 있다.
+//
+// 6	8	2	6	2
+// 3	2	3	4	6
+// 6	7	3	3	2
+// 7	2	5	3	6
+// 8	9	5	2	7
+// 이와 같이 장마철에 내리는 비의 양에 따라서 물에 잠기지 않는 안전한 영역의 개수는 다르게 된다.
+// 위의 예와 같은 지역에서 내리는 비의 양에 따른 모든 경우를 다 조사해 보면 물에 잠기지 않는 안전한 영역의 개수 중에서 최대인 경우는 5임을 알 수 있다.
+//
+// 어떤 지역의 높이 정보가 주어졌을 때, 장마철에 물에 잠기지 않는 안전한 영역의 최대 개수를 계산하는 프로그램을 작성하시오.
 //
 // 입력
-// 첫째 줄에 F, S, G, U, D가 주어진다. (1 ≤ S, G ≤ F ≤ 1000000, 0 ≤ U, D ≤ 1000000) 건물은 1층부터 시작하고, 가장 높은 층은 F층이다.
+// 첫째 줄에는 어떤 지역을 나타내는 2차원 배열의 행과 열의 개수를 나타내는 수 N이 입력된다. N은 2 이상 100 이하의 정수이다.
+// 둘째 줄부터 N개의 각 줄에는 2차원 배열의 첫 번째 행부터 N번째 행까지 순서대로 한 행씩 높이 정보가 입력된다.
+// 각 줄에는 각 행의 첫 번째 열부터 N번째 열까지 N개의 높이 정보를 나타내는 자연수가 빈 칸을 사이에 두고 입력된다. 높이는 1이상 100 이하의 정수이다.
 //
 // 출력
-// 첫째 줄에 강호가 S층에서 G층으로 가기 위해 눌러야 하는 버튼의 수의 최솟값을 출력한다. 만약, 엘리베이터로 이동할 수 없을 때는 "use the stairs"를 출력한다.
+// 첫째 줄에 장마철에 물에 잠기지 않는 안전한 영역의 최대 개수를 출력한다.
 
 import java.util.*;
 
 public class Main {
 
-    private static int F, S, G, U, D;
+    private static int N;
+    private static List<ArrayList<Integer>> list = new ArrayList<>();
+    private static int[] dx = {1,-1,0,0};
+    private static int[] dy = {0,0,-1,1};
+    private static int answer = 0;
+    private static int max = 0;
+    private static int min = 101;
 
     public static void main(String[] args) {
         solution();
@@ -26,54 +60,54 @@ public class Main {
     private static void solution() {
         Scanner sc = new Scanner(System.in);
 
-        String[] input = sc.nextLine().split(" ");
-        F = Integer.parseInt(input[0]);
-        S = Integer.parseInt(input[1]);
-        G = Integer.parseInt(input[2]);
-        U = Integer.parseInt(input[3]);
-        D = Integer.parseInt(input[4]);
+        N = sc.nextInt();
+        sc.nextLine();
 
-        int[] visit = new int[F+1];
-        int answer = bfs(visit);
+        for(int i = 0; i < N; i ++) {
+            list.add(new ArrayList<>());
+            String[] input = sc.nextLine().split(" ");
 
-        if(S == G) {
-            System.out.println(0);
-        } else if(answer == 0) {
-            System.out.println("use the stairs");
-        } else {
-            System.out.println(answer);
+            for(int j = 0; j < N; j ++) {
+                list.get(i).add(Integer.parseInt(input[j]));
+                max = Math.max(max, list.get(i).get(j));
+                min = Math.min(min, list.get(i).get(j));
+            }
         }
+
+        if(min == max) {
+            System.out.println(1);
+            return;
+        }
+
+        for(int k = min; k <= max; k ++) {
+            boolean[][] visit = new boolean[N+1][N+1];
+            int cnt = 0;
+            for(int i = 0; i < N; i ++) {
+                for(int j = 0; j < N; j ++) {
+                    if(list.get(i).get(j) > k && !visit[i][j]) {
+                        dfs(i,j,k,visit);
+                        cnt ++;
+                    }
+                }
+            }
+            answer = Math.max(answer, cnt);
+        }
+
+        System.out.println(answer);
     }
 
-    private static int bfs(int[] visit) {
+    private static void dfs(int x, int y, int h, boolean[][] visit) {
 
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(S);
-        visit[S] = 1;
+        visit[x][y] = true;
+        for(int k = 0; k < 4; k ++) {
+            int nx = x + dx[k];
+            int ny = y + dy[k];
 
-        while(!q.isEmpty()) {
-            int cur = q.poll();
-
-            int up = cur + U;
-            int down = cur - D;
-
-            if(up == G || down == G) {
-                visit[G] = visit[cur];
-                break;
-            }
-
-            if(up > 0 && up <= F && visit[up] == 0) {
-                q.offer(up);
-                visit[up] = visit[cur] + 1;
-            }
-
-            if(down > 0 && down <= F && visit[down] == 0) {
-                q.offer(down);
-                visit[down] = visit[cur] + 1;
-            }
+            if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+            if(list.get(nx).get(ny) <= h) continue;
+            if(visit[nx][ny] == true) continue;
+            dfs(nx, ny, h, visit);
         }
-
-        return visit[G];
     }
 
 }
